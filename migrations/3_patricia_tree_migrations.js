18e37db5a0e33da0f749849b0c96622b275d1266
CJ42 <cavallerajean@gmail.com>
2021-08-17 11:08:21 +0100

    remove patricia-tree migrations

--- migrations/3_patricia_tree_migrations.js
+++
@@ -1,9 +0,0 @@
-const PatriciaTree = artifacts.require('PatriciaTree')
-const NFTStoragePatricia = artifacts.require('NFTStoragePatricia')
-
-module.exports = function (deployer, network, accounts) {
-    let owner = accounts[0]
-    deployer.deploy(PatriciaTree)
-    deployer.link(PatriciaTree, NFTStoragePatricia)
-    deployer.deploy(NFTStoragePatricia, owner)
-}
