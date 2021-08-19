bd9ee38d7e4f2d80d2697b65d4349bae3f953d71
Reto Ryter <reto@twy.gmbh>
2021-08-19 19:56:51 +0000

    chore(hardhat): migrate NFTStorage tests

--- test/NFTStorage.spec.ts
+++ test/NFTStorage.spec.ts
@@ -1,34 +1,35 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { Signer } from "ethers";
+import { keccak256 } from "ethers/lib/utils";
 import { ethers } from "hardhat";
+import { NFTStorageMerkle, NFTStorageMerkle__factory } from "../build/types";
 
 const { MerkleTree } = require("merkletreejs");
 
 describe("NFTStorageMerkle", () => {
   describe("Testing Merkle Tree", () => {
     let accounts: SignerWithAddress[];
+    let owner, nftList;
     beforeEach(async () => {
       accounts = await ethers.getSigners();
+      owner = accounts[0];
+      nftList = [
+        accounts[1].address,
+        accounts[2].address,
+        accounts[3].address,
+        accounts[4].address, // verify this leaf
+        accounts[5].address,
+        accounts[6].address,
+        accounts[7].address,
+        accounts[8].address,
+      ];
     });
 
-    let owner = accounts[0];
-    let nftList = [
-      accounts[1],
-      accounts[2],
-      accounts[3],
-      accounts[4], // verify this leaf
-      accounts[5],
-      accounts[6],
-      accounts[7],
-      accounts[8],
-    ];
-
-    let nftStorage;
+    let nftStorage: NFTStorageMerkle;
     let leaves;
     let merkletree;
 
-    before(async () => {
-      nftStorage = await NFTStorageMerkle.new({ from: owner });
+    beforeEach(async () => {
+      nftStorage = await new NFTStorageMerkle__factory(owner).deploy();
 
       leaves = nftList.map((x) => keccak256(x));
       merkletree = new MerkleTree(leaves, keccak256, { sortPairs: true });
@@ -40,22 +41,22 @@ describe("NFTStorageMerkle", () => {
 
     it("Should return 8 for leaves count", async () => {
       let count = merkletree.getHexLeaves().length;
-      assert.equal(count, 8, "not the same number of leaves");
+      expect(count).toEqual(8);
     });
 
     it("Keccak256 hash should match for the first NFT address", async () => {
       let firstNFT = merkletree.getHexLeaves()[0];
 
-      assert.equal(firstNFT, web3.utils.sha3(nftList[0]), "Not the same keccak256 hash");
+      expect(firstNFT).toEqual(keccak256(nftList[0]));
     });
 
     it("Should verify the proof in the smart contract", async () => {
-      let root = merkletree.getHexRoot();
-      let leaf = merkletree.getHexLeaves()[3];
-      let proof = merkletree.getHexProof(leaf);
+      let root = merkletree.getHexRoot(); // .?
+      let leaf = merkletree.getHexLeaves()[3]; // ?.
+      let proof = merkletree.getHexProof(leaf); // .
 
-      let result = await nftStorage.verifyMerkleProof(proof, root, leaf);
-      assert.isTrue(result, "Merkle Proof invalid");
+      let result = await nftStorage.callStatic.verifyMerkleProof(proof, root, leaf);
+      expect(result).toBeTruthy();
     });
   });
 });
