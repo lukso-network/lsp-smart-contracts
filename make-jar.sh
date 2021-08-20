3df90d3bf0fb3c4487207280f5f9065d854df2c0
Leonard Schellenberg <leonard.schellenberg@gmail.com>
2021-08-20 14:00:05 +0200

    fixed error

--- make-jar.sh
+++ make-jar.sh
@@ -10,8 +10,7 @@ curl -L get.web3j.io | sh && source ~/.web3j/source.sh
 
 solc --abi --bin @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ ../submodules="$(pwd)"/submodules solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ -o ./output/ contracts/LSP3Account.sol contracts/KeyManager/KeyManager.sol contracts/UniversalReceiver/BasicUniversalReceiver.sol
 
-export destination=./java/src/main/java/network/up/contracts
-export destination=.java/src/main/java/network/up/contracts
+export destination=./java/src/main/java/network/lukso/up/contracts
 export package=network.lukso.up.contracts
 
 FILES=(KeyManager LSP3Account BasicUniversalReceiver ERC725Account)
@@ -34,10 +33,3 @@ web3j generate solidity --abiFile=$abiSource --binFile=$binSource -o $destinatio
 
 rm -Rf ./output
 
-export inputsPath=./src/main/java/network/lukso/up/contracts/
-
-mkdir jar
-jar cf ./jar/BasicUniversalReceiver.jar $inputsPath/BasicUniversalReceiver.java
-jar cf ./jar/ERC725Account.jar $inputsPath/ERC725Account.java
-jar cf ./jar/KeyManager.jar $inputsPath/KeyManager.java
-jar cf ./jar/LSP3Account.jar $inputsPath/LSP3Account.java
