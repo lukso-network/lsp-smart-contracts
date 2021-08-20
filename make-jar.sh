826a3bdeb22d8f4a88ebf13bbd2cca09c82addb2
CJ42 <cavallerajean@gmail.com>
2021-08-20 10:20:05 +0100

    chore(cd): fetch web3j

--- make-jar.sh
+++ make-jar.sh
@@ -5,6 +5,8 @@ set -e
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install solc
+# Install web3j
+curl -L get.web3j.io | sh && source ~/.web3j/source.sh
 
 solc --abi --bin @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ ../submodules="$(pwd)"/submodules solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ -o ./output/ contracts/LSP3Account.sol contracts/KeyManager/KeyManager.sol contracts/UniversalReceiver/BasicUniversalReceiver.sol
 
