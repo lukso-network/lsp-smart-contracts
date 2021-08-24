e2ae4e33c2b476813fdac16b468b593f85af7f26
CJ42 <cavallerajean@gmail.com>
2021-08-24 18:12:21 +0100

    chore(cd): iOS workflow test

--- make-ios.js
+++ make-ios.js
@@ -2,13 +2,11 @@
 
 const fs = require('fs');
 
-// 1) Get content of the JSON files
 const erc725Account = fs.readFileSync('./build/contracts/ERC725.json')
 const lsp3Account = fs.readFileSync('./build/contracts/LSP3Account.json')
 const universalReceiver = fs.readFileSync('./build/contracts/BasicUniversalReceiver.json')
 const keyManager = fs.readFileSync('./build/contracts/KeyManager.json')
 
-// 2) Inside these files, grab the `.abi` object
 const erc725ABI = JSON.parse(erc725Account).abi;
 const lsp3ABI = JSON.parse(lsp3Account).abi;
 const universalReceiverABI = JSON.parse(universalReceiver).abi;
@@ -26,7 +24,6 @@ public final class UpContractAbi : String {
 }
 `
 
-// 3) Save to these files
 fs.appendFile(upSwiftFile, fileContents, (err) => {
     if (err) console.log(err)
 });
