95e511f1ecc693ca32b6f886dbaa0c606c4d1279
CJ42 <CJ42@users.noreply.github.com>
2021-08-31 17:27:55 +0000

    Prettified Code!

--- migrations/2_erc725_migrations.js
+++ migrations/2_erc725_migrations.js
@@ -1,21 +1,20 @@
-const KeyManager = artifacts.require("KeyManager")
-const KeyManagerHelper = artifacts.require("KeyManagerHelper")
+const KeyManager = artifacts.require("KeyManager");
+const KeyManagerHelper = artifacts.require("KeyManagerHelper");
 
-const ERC725Utils = artifacts.require("ERC725Utils")
-const TargetContract = artifacts.require("TargetContract")
+const ERC725Utils = artifacts.require("ERC725Utils");
+const TargetContract = artifacts.require("TargetContract");
 
 module.exports = function (deployer, network, accounts) {
+  let owner = accounts[0];
+  deployer.deploy(ERC725Utils);
 
-    let owner = accounts[0]
-    deployer.deploy(ERC725Utils)
+  // BasicKeyManager = old version
+  // KeyManager = new version
+  deployer.link(ERC725Utils, KeyManager);
+  deployer.link(ERC725Utils, KeyManagerHelper);
 
-    // BasicKeyManager = old version
-    // KeyManager = new version
-    deployer.link(ERC725Utils, KeyManager)
-    deployer.link(ERC725Utils, KeyManagerHelper)
+  deployer.deploy(KeyManager, owner);
+  deployer.deploy(KeyManagerHelper, owner);
 
-    deployer.deploy(KeyManager, owner)
-    deployer.deploy(KeyManagerHelper, owner)
-
-    deployer.deploy(TargetContract)
+  deployer.deploy(TargetContract);
 };
