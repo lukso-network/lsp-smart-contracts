edd44d900579f7e19ef299f2a436a6e65264bc59
CJ42 <cavallerajean@gmail.com>
2021-09-16 08:53:44 +0100

    Show different calldata payloads for setData

--- tests/ERC725Account.spec.ts
+++ tests/ERC725Account.spec.ts
@@ -2,8 +2,23 @@ import { encodeData, ERC725JSONSchema, flattenEncodedData, KeyValuePair } from "
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers } from "hardhat";
 import { ERC725Account, ERC725Account__factory } from "../build/types";
+import { getRandomAddresses, generateKeysAndValues } from "./utils/helpers";
 
-const schema: ERC725JSONSchema[] = [
+const SCHEMA: ERC725JSONSchema[] = [
+  {
+    name: "LSP3Profile",
+    key: "0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5",
+    keyType: "Singleton",
+    valueContent: "JSONURL",
+    valueType: "bytes",
+  },
+  {
+    name: "LSP1UniversalReceiverDelegate",
+    key: "0x0cfc51aec37c55a4d0b1a65c6255c4bf2fbdf6277f3cc0730c45b828b6db8b47",
+    keyType: "Singleton",
+    valueContent: "Address",
+    valueType: "address",
+  },
   {
     name: "LSP3IssuedAssets[]",
     key: "0x3a47ab5bd3a594c3a8995f8fa58d0876c96819ca4516bd76100c92462f2f9dc0",
@@ -27,46 +42,134 @@ describe("ERC725 Account", () => {
     erc725Account = await new ERC725Account__factory(owner).deploy(owner.address);
   });
 
-  it("should add 100 LSP3IssuedAssets[]", async () => {
-    for (let ii = 0; ii < 50; ii++) {
-      lsp3IssuedAssets.push("0xa56039d89BD9451A1ac94a680a20302da2dE925A");
-    }
+  describe("Display encoded abi for `setData`", () => {
+    it("> one singleton key", async () => {
+      // prettier-ignore
+      let element = { "MyFirstKey": "Hello Lukso!" };
 
-    const data = {
-      "LSP3IssuedAssets[]": lsp3IssuedAssets,
-    };
+      let [keys, values] = generateKeysAndValues(element);
 
-    const encodedData = encodeData(data, schema);
-    const flattenedEncodedData = flattenEncodedData(encodedData);
+      let payload = erc725Account.interface.encodeFunctionData("setData", [keys, values]);
+      console.log("1 singleton key:");
+      console.log(payload);
+    });
 
-    await Promise.all(
-      flattenedEncodedData.map(async (a) => {
-        return erc725Account.setData([a.key], [a.value]);
-      })
-    );
+    it("> 5 x singleton keys(same length)", async () => {
+      // prettier-ignore
+      let elements = {
+          "MyFirstKey":   "aaaaaaaaaa",
+          "MySecondKey":  "bbbbbbbbbb",
+          "MyThirdKey":   "cccccccccc",
+          "MyFourthKey":  "dddddddddd",
+          "MyFifthKey":   "eeeeeeeeee",
+        };
 
-    const [length] = await erc725Account.getData([schema[0].key]);
-    expect(parseInt(length)).toEqual(50);
+      let [keys, values] = generateKeysAndValues(elements);
+
+      let payload = erc725Account.interface.encodeFunctionData("setData", [keys, values]);
+      console.log("5 singleton keys (same length):");
+      console.log(payload);
+    });
+
+    //
+    it("> adding 10 x LSP3IssuedAssets", async () => {
+      lsp3IssuedAssets = getRandomAddresses(10);
+
+      const data = { "LSP3IssuedAssets[]": lsp3IssuedAssets };
+
+      const encodedData = encodeData(data, SCHEMA);
+      const flattenedEncodedData = flattenEncodedData(encodedData);
+
+      let keys = [];
+      let values = [];
+
+      flattenedEncodedData.map((data) => {
+        keys.push(data.key);
+        values.push(data.value);
+      });
+
+      let payload = erc725Account.interface.encodeFunctionData("setData", [keys, values]);
+      console.log("20 x LSP3IssuedAssets:");
+      console.log(payload);
+    });
+
+    it("> basic Universal Profile setup (3 keys) => set `LSP3Profile`, `LSP3IssuedAssets[]` and `LSP1UniversalReceiverDelegate`", async () => {
+      const basicUPSetup = {
+        LSP3Profile: {
+          hashFunction: "keccak256(utf8)",
+          hash: "0x820464ddfac1bec070cc14a8daf04129871d458f2ca94368aae8391311af6361",
+          url: "ifps://QmYr1VJLwerg6pEoscdhVGugo39pa6rycEZLjtRPDfW84UAx",
+        },
+        "LSP3IssuedAssets[]": [
+          "0xD94353D9B005B3c0A9Da169b768a31C57844e490",
+          "0xDaea594E385Fc724449E3118B2Db7E86dFBa1826",
+        ],
+        LSP1UniversalReceiverDelegate: "0x1183790f29BE3cDfD0A102862fEA1a4a30b3AdAb",
+      };
+
+      let encodedData = encodeData(basicUPSetup, SCHEMA);
+      let flattenedEncodedData = flattenEncodedData(encodedData);
+
+      let keys = [];
+      let values = [];
+
+      flattenedEncodedData.map((data) => {
+        keys.push(data.key);
+        values.push(data.value);
+      });
+
+      let payload = erc725Account.interface.encodeFunctionData("setData", [keys, values]);
+      console.log("Basic UP Setup ==> 3 items (different length):");
+      console.log(payload);
+    });
   });
 
-  it("Add one more LSP3IssuedAssets[]", async () => {
-    let newAsset = "0xa56039d89BD9451A1ac94a680a20302da2dE925A";
-    lsp3IssuedAssets.push(newAsset);
+  describe("Adding LSP3IssuedAssets", () => {
+    it("should add 50 LSP3IssuedAssets[]", async () => {
+      lsp3IssuedAssets = getRandomAddresses(50);
 
-    const data = {
-      "LSP3IssuedAssets[]": lsp3IssuedAssets,
-    };
+      const data = { "LSP3IssuedAssets[]": lsp3IssuedAssets };
 
-    const encodedData = encodeData(data, schema);
-    const flattenedEncodedData = flattenEncodedData(encodedData);
+      const encodedData = encodeData(data, SCHEMA);
+      const flattenedEncodedData = flattenEncodedData(encodedData);
 
-    await Promise.all(
-      flattenedEncodedData.map(async (a) => {
-        return erc725Account.setData([a.key], [a.value]);
-      })
-    );
+      let keys = [];
+      let values = [];
 
-    const [length] = await erc725Account.getData([schema[0].key]);
-    expect(parseInt(length)).toEqual(51);
+      flattenedEncodedData.map((data) => {
+        keys.push(data.key);
+        values.push(data.value);
+      });
+
+      await erc725Account.setData(keys, values);
+
+      const [length] = await erc725Account.getData([SCHEMA[2].key]);
+      expect(parseInt(length)).toEqual(50);
+    });
+
+    it("Add one more LSP3IssuedAssets[]", async () => {
+      let newAsset = "0xa56039d89BD9451A1ac94a680a20302da2dE925A";
+      lsp3IssuedAssets.push(newAsset);
+
+      const data = {
+        "LSP3IssuedAssets[]": lsp3IssuedAssets,
+      };
+
+      const encodedData = encodeData(data, SCHEMA);
+      const flattenedEncodedData = flattenEncodedData(encodedData);
+
+      let keys = [];
+      let values = [];
+
+      flattenedEncodedData.map((data) => {
+        keys.push(data.key);
+        values.push(data.value);
+      });
+
+      await erc725Account.setData(keys, values);
+
+      const [length] = await erc725Account.getData([SCHEMA[2].key]);
+      expect(parseInt(length)).toEqual(51);
+    });
   });
 });
--- tests/utils/helpers.ts
+++ tests/utils/helpers.ts
@@ -1,3 +1,4 @@
+import { ethers } from "hardhat";
 import { Contract, ContractTransaction } from "ethers";
 
 export async function getDeploymentCost(contractOrTransaction: Contract | ContractTransaction) {
@@ -15,4 +16,30 @@ export async function getDeploymentCost(contractOrTransaction: Contract | Contra
     receipt,
     gasUsed,
   };
+}
+
+export function getRandomAddresses(count) {
+  let base = "0xa56039d89BD9451A1ac94a680a20302da2dE92";
+
+  let addresses = [];
+  for (let ii = 10; ii < count + 10; ii++) {
+    let randomAddress = "0x" + parseInt(base + ii).toString(16);
+    addresses.push(randomAddress);
+  }
+
+  return addresses;
+}
+
+export function generateKeysAndValues(_elementObject) {
+  let keys = [];
+  let values = [];
+  for (const [_key, _value] of Object.entries(_elementObject)) {
+    let key = ethers.utils.toUtf8Bytes(_key);
+    let value = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(_value));
+
+    keys.push(ethers.utils.keccak256(key));
+    values.push(value);
+  }
+
+  return [keys, values];
 }
