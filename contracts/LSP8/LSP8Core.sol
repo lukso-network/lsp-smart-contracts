8a4a959e351cb6e3b4c16e8b8a1016b5ff27bd94
mattgstevens <mattgstevens@gmail.com>
2021-09-23 11:52:23 +0000

    refactor: create LSP4Constants for constant bytes32 keys for ERC725Y storage

--- contracts/LSP8/LSP8Core.sol
+++ contracts/LSP8/LSP8Core.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0;
 
+// constants
+import "../LSP4/LSP4Constants.sol";
+
 // interfaces
 import "../_LSPs/ILSP1_UniversalReceiver.sol";
 import "./ILSP8.sol";
@@ -24,15 +27,6 @@ abstract contract LSP8Core is Context, ERC725YCore, ILSP8 {
     using EnumerableSet for EnumerableSet.AddressSet;
     using EnumerableSet for EnumerableSet.Bytes32Set;
     using Address for address;
-    //
-    // --- Constants
-    //
-
-    // keccak256('LSP4TokenName')
-    bytes32 constant _LSP4_METADATA_TOKEN_NAME_KEY = 0xdeba1e292f8ba88238e10ab3c7f88bd4be4fac56cad5194b6ecceaf653468af1;
-
-    // keccak256('LSP4TokenSymbol')
-    bytes32 constant _LSP4_METADATA_TOKEN_SYMBOL_KEY = 0x2f0a68ab07768e01943a599e73362a0e17a63a72e94dd2e384d2c1d4db932756;
 
     //
     // --- Storage: Fixed
