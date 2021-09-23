1c7c980e556df4749189adeffbbc6e99fd6d8324
mattgstevens <mattgstevens@gmail.com>
2021-09-23 11:52:23 +0000

    refactor: create LSP4Constants for constant bytes32 keys for ERC725Y storage

--- contracts/LSP4/LSP4Compatibility.sol
+++ contracts/LSP4/LSP4Compatibility.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
+
+// constants
+import "./LSP4Constants.sol";
 
 // interfaces
 import "./ILSP4Compatibility.sol";
@@ -12,16 +14,6 @@ import "../../submodules/ERC725/implementations/contracts/ERC725/ERC725Y.sol";
  * @dev LSP4 extension, for compatibility with clients & tools that expect ERC20/721.
  */
 abstract contract LSP4Compatibility is ERC725Y, ILSP4Compatibility {
-    //
-    // --- Constants
-    //
-
-    // keccak256('LSP4TokenName')
-    bytes32 constant _LSP4_METADATA_TOKEN_NAME_KEY = 0xdeba1e292f8ba88238e10ab3c7f88bd4be4fac56cad5194b6ecceaf653468af1;
-
-    // keccak256('LSP4TokenSymbol')
-    bytes32 constant _LSP4_METADATA_TOKEN_SYMBOL_KEY = 0x2f0a68ab07768e01943a599e73362a0e17a63a72e94dd2e384d2c1d4db932756;
-
     //
     // --- Token queries
     //
