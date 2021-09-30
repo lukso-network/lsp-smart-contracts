c04d0143aa8e437cad0e27b0803b4b0212586cd2
CJ42 <cavallerajean@gmail.com>
2021-09-30 15:19:20 +0100

    fix: merge conflicts develop > lsp3-core

--- contracts/UniversalProfile.sol
+++ contracts/UniversalProfile.sol
@@ -2,26 +2,26 @@
 pragma solidity ^0.8.0;
 
 // interfaces
-import "./_LSPs/ILSP1_UniversalReceiver.sol";
-import "./_LSPs/ILSP1_UniversalReceiverDelegate.sol";
+// import "./_LSPs/ILSP1_UniversalReceiver.sol";
+// import "./_LSPs/ILSP1_UniversalReceiverDelegate.sol";
 
 // modules
 import "../submodules/ERC725/implementations/contracts/ERC725/ERC725Account.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
 
 // libraries
-import "./Utils/ERC725Utils.sol";
+import "../submodules/ERC725/implementations/contracts/Utils/ERC725Utils.sol";
 
 /**
  * @title implementation of a LUKSO's Universal Profile based on LSP3
  * @author Fabian Vogelsteller <fabian@lukso.network>
  * @dev Implementation of the ERC725Account + LSP1 universalReceiver
  */
-contract UniversalProfile is ERC165Storage, ERC725Account, ILSP1 {
+contract UniversalProfile is ERC165Storage, ERC725Account{
     using ERC725Utils for ERC725Y;
 
-    bytes4 constant _INTERFACE_ID_LSP1 = 0x6bb56a14;
-    bytes4 constant _INTERFACE_ID_LSP1DELEGATE = 0xc2d7bcc1;
+    // bytes4 constant _INTERFACE_ID_LSP1 = 0x6bb56a14;
+    // bytes4 constant _INTERFACE_ID_LSP1DELEGATE = 0xc2d7bcc1;
 
     bytes32 constant private _UNIVERSAL_RECEIVER_DELEGATE_KEY =
         0x0cfc51aec37c55a4d0b1a65c6255c4bf2fbdf6277f3cc0730c45b828b6db8b47; // keccak256("LSP1UniversalReceiverDelegate")
@@ -63,7 +63,7 @@ contract UniversalProfile is ERC165Storage, ERC725Account, ILSP1 {
         external
         override
         virtual
-        returns (bytes32 returnValue)
+        returns (bytes memory returnValue)
     {
         bytes memory receiverData = ERC725Y(this).getDataSingle(_UNIVERSAL_RECEIVER_DELEGATE_KEY);
         returnValue = "";
--- contracts/UniversalProfileInit.sol
+++ contracts/UniversalProfileInit.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 // interfaces
-import "./_LSPs/ILSP1_UniversalReceiver.sol";
-import "./_LSPs/ILSP1_UniversalReceiverDelegate.sol";
+// import "./_LSPs/ILSP1_UniversalReceiver.sol";
+// import "./_LSPs/ILSP1_UniversalReceiverDelegate.sol";
 import "../submodules/ERC725/implementations/contracts/ERC725/ERC725YInit.sol";
 
 // modules
@@ -12,18 +12,18 @@ import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
 import "solidity-bytes-utils/contracts/BytesLib.sol";
 
 // libraries
-import "./Utils/ERC725Utils.sol";
+import "../submodules/ERC725/implementations/contracts/Utils/ERC725Utils.sol";
 
 /**
  * @title Proxy implementation of a LUKSO's Universal Profile based on LSP3
  * @author Fabian Vogelsteller <fabian@lukso.network>
  * @dev Implementation of the ERC725Account + LSP1 universalReceiver
  */
-contract UniversalProfileInit is ERC165Storage, ERC725AccountInit, ILSP1 {
+contract UniversalProfileInit is ERC165Storage, ERC725AccountInit {
     using ERC725Utils for ERC725YInit;
 
-    bytes4 constant _INTERFACE_ID_LSP1 = 0x6bb56a14;
-    bytes4 constant _INTERFACE_ID_LSP1DELEGATE = 0xc2d7bcc1;
+    // bytes4 constant _INTERFACE_ID_LSP1 = 0x6bb56a14;
+    // bytes4 constant _INTERFACE_ID_LSP1DELEGATE = 0xc2d7bcc1;
 
     bytes32 constant private _UNIVERSAL_RECEIVER_DELEGATE_KEY =
         0x0cfc51aec37c55a4d0b1a65c6255c4bf2fbdf6277f3cc0730c45b828b6db8b47; // keccak256("LSP1UniversalReceiverDelegate")
@@ -65,7 +65,7 @@ contract UniversalProfileInit is ERC165Storage, ERC725AccountInit, ILSP1 {
         external
         override
         virtual
-        returns (bytes32 returnValue)
+        returns (bytes memory returnValue)
     {
         bytes memory receiverData = ERC725YInit(this).getDataSingle(_UNIVERSAL_RECEIVER_DELEGATE_KEY);
         returnValue = "";
