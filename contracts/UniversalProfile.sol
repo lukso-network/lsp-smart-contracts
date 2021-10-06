5d99672c4d136cf68842271bfc0a0722c35f4aed
CJ42 <cavallerajean@gmail.com>
2021-10-06 19:26:07 +0100

    refactor: Core version of UniversalProfile
    UniversalProfile contract now splitted between Core and Init

--- contracts/UniversalProfile.sol
+++ contracts/UniversalProfile.sol
@@ -1,55 +1,45 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-// interfaces
-// import "./_LSPs/ILSP1_UniversalReceiver.sol";
-// import "./_LSPs/ILSP1_UniversalReceiverDelegate.sol";
+import "./UniversalProfileCore.sol";
 
 // modules
 import "../submodules/ERC725/implementations/contracts/ERC725/ERC725Account.sol";
-import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
 
-// libraries
-import "../submodules/ERC725/implementations/contracts/Utils/ERC725Utils.sol";
 
 /**
  * @title implementation of a LUKSO's Universal Profile based on LSP3
  * @author Fabian Vogelsteller <fabian@lukso.network>
  * @dev Implementation of the ERC725Account + LSP1 universalReceiver
  */
-contract UniversalProfile is ERC165Storage, ERC725Account{
-    using ERC725Utils for ERC725Y;
-
-    // bytes4 constant _INTERFACE_ID_LSP1 = 0x6bb56a14;
-    // bytes4 constant _INTERFACE_ID_LSP1DELEGATE = 0xc2d7bcc1;
-
-    bytes32 constant private _UNIVERSAL_RECEIVER_DELEGATE_KEY =
-        0x0cfc51aec37c55a4d0b1a65c6255c4bf2fbdf6277f3cc0730c45b828b6db8b47; // keccak256("LSP1UniversalReceiverDelegate")
-
-    bytes32[] public dataKeys;
+contract UniversalProfile is ERC725Account, UniversalProfileCore  {
 
     constructor(address _newOwner) ERC725Account(_newOwner) {
+        
+        // set SupportedStandards:LSP3UniversalProfile
+        bytes32 key = 0xeafec4d89fa9619884b6b89135626455000000000000000000000000abe425d6;
+        store[key] = abi.encodePacked(bytes4(0xabe425d6));
+        dataKeys.push(key);
+        emit DataChanged(key, store[key]);
+
         _registerInterface(_INTERFACE_ID_LSP1);
     }
 
-    /* non-standard public functions */
-
-    function allDataKeys() public view returns (bytes32[] memory) {
-        return dataKeys;
+    function execute(
+        uint256 _operation,
+        address _to,
+        uint256 _value,
+        bytes calldata _data
+    ) public payable virtual override(ERC725Account, ERC725XCore) onlyOwner returns(bytes memory result) {
+        result = ERC725Account.execute(_operation,_to,_value,_data);
     }
 
     function setData(bytes32[] calldata _keys, bytes[] calldata _values)
         public
-        override
+        override(UniversalProfileCore, ERC725Account)
         onlyOwner
     {
-        require(_keys.length == _values.length, "Keys length not equal to values length");
-        for (uint256 ii = 0; ii < _keys.length; ii++) {
-            if (store[_keys[ii]].length == 0) {
-                dataKeys.push(_keys[ii]);
-            }
-            _setData(_keys[ii], _values[ii]);
-        }
+        UniversalProfileCore.setData(_keys, _values);
     }
 
 }
