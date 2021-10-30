7d344693ae0ccf099019d0bf8455f2a596b5e4bd
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2021-10-30 15:36:24 +0100

    Add Proxy + Token Contracts to artifacts (#37)
    * refactor(UP): :recycle: UP deployment code to save few gas
    
    store variable as hex literal to save +- 1300 gas
    
    * chore: add proxy + tokens to package build
    
    * chore: add proxy + tokens to android build
    
    * ci: :bug: fix iOS ci -> build ABIs before writing to SWIFT file
    
    Adding extra step to generate the builds
    
    * chore: :iphone: add proxy + tokens artifacts to iOS
    
    * ci: fix typo in iOS workflow
    
    * chore: remove URAddressStore from build
    
    * chore: :package: add UniversalReceiverDelegate to artifacts (npm, ios, android)
    
    Co-authored-by: YamenMerhi

--- make-jar.sh
+++ make-jar.sh
@@ -8,27 +8,55 @@ sudo apt-get install solc
 
 curl -L get.web3j.io | sh && source ~/.web3j/source.sh
 
-solc --abi --bin @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ ../submodules="$(pwd)"/submodules solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ -o ./output/ contracts/UniversalProfile.sol contracts/KeyManager/KeyManager.sol contracts/UniversalReceiver/BasicUniversalReceiver.sol
-
 export destination=./java/src/main/java/network/lukso/up/contracts
 export package=network.lukso.up.contracts
 
-FILES=(KeyManager UniversalProfile BasicUniversalReceiver ERC725Account)
+solc --abi --bin \
+    solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ \
+    @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ \
+    ../submodules="$(pwd)"/submodules \
+    contracts/UniversalProfile.sol \
+    contracts/LSP6-KeyManager/KeyManager.sol \
+    contracts/LSP1-UniversalReceiver/UniversalReceiverDelegate.sol \
+    contracts/LSP7-DigitalAsset/LSP7.sol \
+    contracts/LSP7-DigitalAsset/extensions/LSP7CappedSupply.sol \
+    contracts/LSP8-IdentifiableDigitalAsset/LSP8.sol \
+    contracts/LSP8-IdentifiableDigitalAsset/extensions/LSP8CappedSupply.sol \
+    contracts/UniversalProfileInit.sol \
+    contracts/LSP6-KeyManager/KeyManagerInit.sol \
+    contracts/LSP1-UniversalReceiver/UniversalReceiverDelegateInit.sol \
+    contracts/LSP7-DigitalAsset/LSP7Init.sol \
+    contracts/LSP7-DigitalAsset/extensions/LSP7CappedSupplyInit.sol \
+    contracts/LSP8-IdentifiableDigitalAsset/LSP8Init.sol \
+    contracts/LSP8-IdentifiableDigitalAsset/extensions/LSP8CappedSupplyInit.sol \
+    contracts/LSP7-DigitalAsset/extensions/LSP7CompatibilityForERC20.sol \
+    contracts/LSP8-IdentifiableDigitalAsset/extensions/LSP8CompatibilityForERC721.sol \
+    -o ./output/
 
-export binSource=./output/UniversalProfile.bin # source of compiled contracts
-export abiSource=./output/UniversalProfile.abi
-web3j generate solidity --abiFile=$abiSource --binFile=$binSource -o $destination -p $package
+FILES=(
+    UniversalProfile 
+    KeyManager 
+    UniversalReceiverDelegate
+    LSP7 
+    LSP7CappedSupply 
+    LSP8 
+    LSP8CappedSupply
+    UniversalProfileInit
+    KeyManagerInit
+    UniversalReceiverDelegateInit
+    LSP7Init
+    LSP7CappedSupplyInit
+    LSP8Init
+    LSP8CappedSupplyInit
+    LSP7CompatibilityForERC20
+    LSP8CompatibilityForERC721
+)
 
-export binSource=./output/KeyManager.bin # source of compiled contracts
-export abiSource=./output/KeyManager.abi
-web3j generate solidity --abiFile=$abiSource --binFile=$binSource -o $destination -p $package
-
-export binSource=./output/BasicUniversalReceiver.bin # source of compiled contracts
-export abiSource=./output/BasicUniversalReceiver.abi
-web3j generate solidity --abiFile=$abiSource --binFile=$binSource -o $destination -p $package
-
-export binSource=./output/ERC725Account.bin # source of compiled contracts
-export abiSource=./output/ERC725Account.abi
-web3j generate solidity --abiFile=$abiSource --binFile=$binSource -o $destination -p $package
+for (( ii=0; ii<${#FILES[@]}; ii++ ));
+do
+    export binSource=./output/${FILES[$ii]}.bin # source of compiled contracts
+    export abiSource=./output/${FILES[$ii]}.abi
+    web3j generate solidity --abiFile=$abiSource --binFile=$binSource -o $destination -p $package
+done
 
 rm -Rf ./output
