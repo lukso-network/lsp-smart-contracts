097bf683138be59f348797127a118d8d5702b664
CJ42 <cavallerajean@gmail.com>
2021-11-12 16:08:14 +0000

    feat!: :sparkles: make ERC20 / 721 compatible versions of LSP7/8 deployable

--- contracts/Helpers/Tokens/LSP8CompatibilityForERC721Tester.sol
+++ contracts/Helpers/Tokens/LSP8CompatibilityForERC721Tester.sol
@@ -6,15 +6,19 @@ pragma solidity ^0.8.0;
 import "../../LSP8IdentifiableDigitalAsset/extensions/LSP8CompatibilityForERC721.sol";
 import "../../LSP8IdentifiableDigitalAsset/LSP8IdentifiableDigitalAsset.sol";
 
-contract LSP8CompatibilityForERC721Tester is LSP8IdentifiableDigitalAsset, LSP8CompatibilityForERC721 {
+contract LSP8CompatibilityForERC721Tester is LSP8CompatibilityForERC721 {
     /* solhint-disable no-empty-blocks */
     constructor(
-      string memory name,
-      string memory symbol,
-      address newOwner
-    ) LSP8IdentifiableDigitalAsset(name, symbol, newOwner) {}
+        string memory name,
+        string memory symbol,
+        address newOwner
+    ) LSP8CompatibilityForERC721(name, symbol, newOwner) {}
 
-    function mint(address to, uint256 tokenId, bytes calldata data) public {
+    function mint(
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) public {
         // using force=true so we can send to EOA in test
         _mint(to, bytes32(tokenId), true, data);
     }
--- contracts/LSP7DigitalAsset/extensions/LSP7CompatibilityForERC20.sol
+++ contracts/LSP7DigitalAsset/extensions/LSP7CompatibilityForERC20.sol
@@ -11,15 +11,17 @@ import "./ILSP7CompatibilityForERC20.sol";
 /**
  * @dev LSP7 extension, for compatibility for clients / tools that expect ERC20.
  */
-abstract contract LSP7CompatibilityForERC20 is ILSP7CompatibilityForERC20, LSP7DigitalAssetCore {
+contract LSP7CompatibilityForERC20 is ILSP7CompatibilityForERC20, LSP7DigitalAsset {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address newOwner_
+    ) LSP7DigitalAsset(name_, symbol_, newOwner_, false) {}
+
     /*
      * @dev Compatible with ERC20 approve.
      */
-    function approve(address operator, uint256 amount)
-        external
-        virtual
-        override
-    {
+    function approve(address operator, uint256 amount) external virtual override {
         return authorizeOperator(operator, amount);
     }
 
@@ -40,11 +42,7 @@ abstract contract LSP7CompatibilityForERC20 is ILSP7CompatibilityForERC20, LSP7D
      * @dev Compatible with ERC20 transfer.
      * Using force=true so that EOA and any contract may receive the tokens.
      */
-    function transfer(address to, uint256 amount)
-        external
-        virtual
-        override
-    {
+    function transfer(address to, uint256 amount) external virtual override {
         return transfer(_msgSender(), to, amount, true, "compat-transfer");
     }
 
@@ -52,11 +50,11 @@ abstract contract LSP7CompatibilityForERC20 is ILSP7CompatibilityForERC20, LSP7D
      * @dev Compatible with ERC20 transferFrom.
      * Using force=true so that EOA and any contract may receive the tokens.
      */
-    function transferFrom(address from, address to, uint256 amount)
-        external
-        virtual
-        override
-    {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external virtual override {
         return transfer(from, to, amount, true, "compat-transferFrom");
     }
 }
