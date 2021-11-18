67c7f4af16ea8f3c654647a8b7f9cc4ab72e3582
CJ42 <cavallerajean@gmail.com>
2021-11-18 17:19:47 +0000

    refactor: Set `SupportedStandards:LSP4DigitalAsset` in LSP4 constructor / initializer
    See LSP4 specs for more infos

--- contracts/LSP4DigitalAssetMetadata/LSP4Constants.sol
+++ contracts/LSP4DigitalAssetMetadata/LSP4Constants.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0;
 
+// --- ERC725Y entries
+
+// bytes16(keccak256('SupportedStandard')) + bytes12(0) + bytes4(keccak256('LSP4DigitalAsset'))
+bytes32 constant _LSP4_SUPPORTED_STANDARDS_KEY = 0xeafec4d89fa9619884b6b89135626455000000000000000000000000a4d96624;
+
+// bytes4(keccak256('LSP4DigitalAsset'))
+bytes constant _LSP4_SUPPORTED_STANDARDS_VALUE = hex"a4d96624";
+
 // keccak256('LSP4TokenName')
-bytes32 constant _LSP4_METADATA_TOKEN_NAME_KEY =
-    0xdeba1e292f8ba88238e10ab3c7f88bd4be4fac56cad5194b6ecceaf653468af1;
+bytes32 constant _LSP4_METADATA_TOKEN_NAME_KEY = 0xdeba1e292f8ba88238e10ab3c7f88bd4be4fac56cad5194b6ecceaf653468af1;
 
 // keccak256('LSP4TokenSymbol')
-bytes32 constant _LSP4_METADATA_TOKEN_SYMBOL_KEY =
-    0x2f0a68ab07768e01943a599e73362a0e17a63a72e94dd2e384d2c1d4db932756;
+bytes32 constant _LSP4_METADATA_TOKEN_SYMBOL_KEY = 0x2f0a68ab07768e01943a599e73362a0e17a63a72e94dd2e384d2c1d4db932756;
