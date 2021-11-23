b8a0bdf50074f79e6e1e020bd489038cddc872e4
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2021-11-23 08:25:38 +0000

    feat: add Mintable version of LSP7 / LSP8  (#61)
    * feat: add `LSP7Mintable` extension
    
    * feat: add `LSP8Mintable` extension
    
    * test: add test to `Mintable` contracts
    
    * test commit
    
    * test: :white_check_mark: fix reference to typescript types
    
    * test: start behaviour test for `LSP7Mintable`
    
    * fix: add missing `initializer` modfier in `LSP8MintableInit`
    
    * test: add test for `LSP8Mintable` extension contract
    
    * test: add tests for LSP7Mintable
    
    * test: use random generated bytes32 for tokenID in LSP8Mintable test
    
    * test: add tests for proxy version of LSP7Mintable
    
    * chore: :rotating_light: fix linter warning (disable empty constructor blocks)
    
    * test: change tests names + variable names
    
    Co-authored-by: YamenMerhi <yamennmerhi@gmail.com>

---
+++ contracts/LSP8IdentifiableDigitalAsset/extensions/LSP8MintableCore.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// modules
+import "../LSP8IdentifiableDigitalAssetCore.sol";
+
+// interfaces
+import "./ILSP8Mintable.sol";
+
+/**
+ * @dev LSP8 extension
+ */
+abstract contract LSP8MintableCore is ILSP8Mintable, LSP8IdentifiableDigitalAssetCore {
+    function mint(
+        address to,
+        bytes32 tokenId,
+        bool force,
+        bytes memory data
+    ) public virtual override {
+        _mint(to, tokenId, force, data);
+    }
+}
---
+++ tests/LSP7DigitalAsset/extensions/LSP7Mintable.behavior.ts
@@ -0,0 +1,76 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { ethers } from "hardhat";
+import { LSP7Mintable } from "../../../types";
+
+export type LSP7MintableTestAccounts = {
+  owner: SignerWithAddress;
+  tokenReceiver: SignerWithAddress;
+};
+
+export const getNamedAccounts = async (): Promise<LSP7MintableTestAccounts> => {
+  const [owner, tokenReceiver] = await ethers.getSigners();
+  return { owner, tokenReceiver };
+};
+
+export type LSP7MintableDeployParams = {
+  name: string;
+  symbol: string;
+  newOwner: string;
+  isNFT: boolean;
+};
+
+export type LSP7MintableTestContext = {
+  accounts: LSP7MintableTestAccounts;
+  lsp7Mintable: LSP7Mintable;
+  deployParams: LSP7MintableDeployParams;
+};
+
+export const shouldBehaveLikeLSP7Mintable = (
+  buildContext: () => Promise<LSP7MintableTestContext>
+) => {
+  let context: LSP7MintableTestContext;
+
+  beforeAll(async () => {
+    context = await buildContext();
+  });
+
+  describe("when owner minting tokens", () => {
+    it("should increase the total supply", async () => {
+      const amountToMint = ethers.BigNumber.from("100");
+      const preTotalSupply = await context.lsp7Mintable.totalSupply();
+
+      await context.lsp7Mintable.mint(
+        context.accounts.tokenReceiver.address,
+        amountToMint,
+        true, // beneficiary is an EOA, so we need to force minting
+        "0x"
+      );
+
+      let postTotalSupply = await context.lsp7Mintable.totalSupply();
+      expect(postTotalSupply).toEqual(preTotalSupply.add(amountToMint));
+    });
+
+    it("should increase the tokenReceiver balance", async () => {
+      const amountToMint = ethers.BigNumber.from("100");
+
+      const tokenReceiverBalance = await context.lsp7Mintable.balanceOf(
+        context.accounts.tokenReceiver.address
+      );
+
+      expect(tokenReceiverBalance.toNumber()).toEqual(amountToMint.toNumber());
+    });
+  });
+
+  describe("when non-owner minting tokens", () => {
+    it("should revert", async () => {
+      const amountToMint = ethers.BigNumber.from("100");
+
+      // use any other account
+      const nonOwner = context.accounts.tokenReceiver;
+
+      await expect(
+        context.lsp7Mintable.connect(nonOwner).mint(nonOwner.address, amountToMint, true, "0x")
+      ).toBeRevertedWith("Ownable: caller is not the owner");
+    });
+  });
+};
