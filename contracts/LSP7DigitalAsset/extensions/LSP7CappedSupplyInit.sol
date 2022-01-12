77b00c6296f9152a5247ff24c049b3edac09d969
Yamen Merhi <yamennmerhi@gmail.com>
2022-01-12 17:45:05 +0200

    fix: fix OZ initialize breaking change

--- contracts/LSP7DigitalAsset/extensions/LSP7CappedSupplyInit.sol
+++ contracts/LSP7DigitalAsset/extensions/LSP7CappedSupplyInit.sol
@@ -3,44 +3,22 @@
 pragma solidity ^0.8.0;
 
 // modules
-import "./LSP7CappedSupplyCore.sol";
-import "../LSP7DigitalAssetInit.sol";
+import "./LSP7CappedSupplyInitAbstract.sol";
 
 /**
  * @dev LSP7 extension, adds token supply cap.
  */
-abstract contract LSP7CappedSupplyInit is
-    Initializable,
-    LSP7DigitalAssetInit,
-    LSP7CappedSupplyCore
-{
+abstract contract LSP7CappedSupplyInit is LSP7CappedSupplyInitAbstract {
     /**
      * @notice Sets the token max supply
      * @param tokenSupplyCap_ The Token max supply
      */
-    function initialize(uint256 tokenSupplyCap_) public virtual initializer {
-        require(tokenSupplyCap_ > 0, "LSP7Capped: tokenSupplyCap is zero");
-        _tokenSupplyCap = tokenSupplyCap_;
-    }
-
-    // --- Overrides
-
-    /**
-     * @dev Mints `amount` tokens and transfers it to `to`.
-     *
-     * Requirements:
-     *
-     * - `tokenSupplyCap() - totalSupply()` must be greater than zero.
-     * - `to` cannot be the zero address.
-     *
-     * Emits a {Transfer} event.
-     */
-    function _mint(
-        address to,
-        uint256 amount,
-        bool force,
-        bytes memory data
-    ) internal virtual override(LSP7DigitalAssetCore, LSP7CappedSupplyCore) {
-        super._mint(to, amount, force, data);
+    function initialize(uint256 tokenSupplyCap_)
+        public
+        virtual
+        override
+        initializer
+    {
+        LSP7CappedSupplyInitAbstract.initialize(tokenSupplyCap_);
     }
 }
--- contracts/LSP8IdentifiableDigitalAsset/LSP8IdentifiableDigitalAssetInit.sol
+++ contracts/LSP8IdentifiableDigitalAsset/LSP8IdentifiableDigitalAssetInit.sol
@@ -2,12 +2,7 @@
 pragma solidity ^0.8.0;
 
 // modules
-import "./LSP8IdentifiableDigitalAssetCore.sol";
-import "../LSP4DigitalAssetMetadata/LSP4DigitalAssetMetadataInit.sol";
-
-// constants
-import "./LSP8Constants.sol";
-import "../LSP4DigitalAssetMetadata/LSP4Constants.sol";
+import "./LSP8IdentifiableDigitalAssetInitAbstract.sol";
 
 /**
  * @title LSP8IdentifiableDigitalAsset contract
@@ -15,23 +10,20 @@ import "../LSP4DigitalAssetMetadata/LSP4Constants.sol";
  * @dev Proxy Implementation of a LSP8 compliant contract.
  */
 contract LSP8IdentifiableDigitalAssetInit is
-    Initializable,
-    LSP4DigitalAssetMetadataInit,
-    LSP8IdentifiableDigitalAssetCore
+    LSP8IdentifiableDigitalAssetInitAbstract
 {
     /**
-     * @notice Sets the token-Metadata and register LSP8InterfaceId
-     * @param name_ The name of the token
-     * @param symbol_ The symbol of the token
-     * @param newOwner_ The owner of the the token-Metadata
+     * @inheritdoc LSP8IdentifiableDigitalAssetInitAbstract
      */
     function initialize(
         string memory name_,
         string memory symbol_,
         address newOwner_
     ) public virtual override initializer {
-        LSP4DigitalAssetMetadataInit.initialize(name_, symbol_, newOwner_);
-
-        _registerInterface(_INTERFACEID_LSP8);
+        LSP8IdentifiableDigitalAssetInitAbstract.initialize(
+            name_,
+            symbol_,
+            newOwner_
+        );
     }
 }
