7f1b66a0aa274949c04c28afe30571e11dc5dccc
YamenMerhi <yamennmerhi@gmail.com>
2022-03-29 12:01:41 +0300

    fix: replace `getDataSingle(...)` by `getData(...)`

--- contracts/LSP1UniversalReceiver/LSP1UniversalReceiverDelegateUP/Handling/TokenAndVaultHandling.sol
+++ contracts/LSP1UniversalReceiver/LSP1UniversalReceiverDelegateUP/Handling/TokenAndVaultHandling.sol
@@ -46,7 +46,8 @@ abstract contract TokenAndVaultHandling {
             mapPrefix,
             bytes20(sender)
         );
-        bytes memory mapValue = IERC725Y(msg.sender).getDataSingle(mapKey);
+        // bytes memory mapValue = IERC725Y(msg.sender).getDataSingle(mapKey);
+        bytes memory mapValue = IERC725Y(msg.sender).getData(mapKey);
 
         if (!senderHook) {
             // if the map is already set, then do nothing
