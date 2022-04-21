b74f02f1d83e0a07a2f35f1748a91eca2b5030f4
CJ42 <cavallerajean@gmail.com>
2022-04-21 12:39:32 +0100

    chore: edit `@author` tag in LSP6 KeyManager conmtracts

--- contracts/LSP6KeyManager/LSP6KeyManagerInit.sol
+++ contracts/LSP6KeyManager/LSP6KeyManagerInit.sol
@@ -6,7 +6,7 @@ import "./LSP6KeyManagerInitAbstract.sol";
 
 /**
  * @title Proxy implementation of a contract acting as a controller of an ERC725 Account, using permissions stored in the ERC725Y storage
- * @author Fabian Vogelsteller, Jean Cavallera
+ * @author Fabian Vogelsteller <frozeman>, Jean Cavallera (CJ42), Yamen Merhi (YamenMerhi)
  * @dev all the permissions can be set on the ERC725 Account using `setData(...)` with the keys constants below
  */
 contract LSP6KeyManagerInit is LSP6KeyManagerInitAbstract {
