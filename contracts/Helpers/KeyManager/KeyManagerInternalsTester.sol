ef8bbbd66e44e6342dd85ad9124c283fef30b25e
CJ42 <cavallerajean@gmail.com>
2022-05-02 11:08:33 +0100

    refactor: rename function account() -> target() in helper contracts

--- contracts/Helpers/KeyManager/KeyManagerInternalsTester.sol
+++ contracts/Helpers/KeyManager/KeyManagerInternalsTester.sol
@@ -13,66 +13,42 @@ contract KeyManagerInternalTester is LSP6KeyManager {
     constructor(address _account) LSP6KeyManager(_account) {}
 
     function getPermissionsFor(address _address) public view returns (bytes32) {
-        return ERC725Y(account).getPermissionsFor(_address);
+        return ERC725Y(target).getPermissionsFor(_address);
     }
 
-    function getAllowedAddressesFor(address _address)
-        public
-        view
-        returns (bytes memory)
-    {
-        return ERC725Y(account).getAllowedAddressesFor(_address);
+    function getAllowedAddressesFor(address _address) public view returns (bytes memory) {
+        return ERC725Y(target).getAllowedAddressesFor(_address);
     }
 
-    function getAllowedFunctionsFor(address _address)
-        public
-        view
-        returns (bytes memory)
-    {
-        return ERC725Y(account).getAllowedFunctionsFor(_address);
+    function getAllowedFunctionsFor(address _address) public view returns (bytes memory) {
+        return ERC725Y(target).getAllowedFunctionsFor(_address);
     }
 
-    function getAllowedERC725YKeysFor(address _address)
-        public
-        view
-        returns (bytes memory)
-    {
-        return ERC725Y(account).getAllowedERC725YKeysFor(_address);
+    function getAllowedERC725YKeysFor(address _address) public view returns (bytes memory) {
+        return ERC725Y(target).getAllowedERC725YKeysFor(_address);
     }
 
-    function verifyAllowedAddress(address _sender, address _recipient)
-        public
-        view
-    {
+    function verifyAllowedAddress(address _sender, address _recipient) public view {
         super._verifyAllowedAddress(_sender, _recipient);
     }
 
-    function verifyAllowedFunction(address _sender, bytes4 _function)
-        public
-        view
-    {
+    function verifyAllowedFunction(address _sender, bytes4 _function) public view {
         super._verifyAllowedFunction(_sender, _function);
     }
 
-    function verifyAllowedERC725YKeys(
-        address _from,
-        bytes32[] memory _inputKeys
-    ) public view {
+    function verifyAllowedERC725YKeys(address _from, bytes32[] memory _inputKeys) public view {
         super._verifyAllowedERC725YKeys(_from, _inputKeys);
     }
 
-    function includesPermissions(
-        bytes32 _addressPermission,
-        bytes32 _permissions
-    ) public pure returns (bool) {
+    function includesPermissions(bytes32 _addressPermission, bytes32 _permissions)
+        public
+        pure
+        returns (bool)
+    {
         return _addressPermission.includesPermissions(_permissions);
     }
 
-    function countZeroBytes(bytes32 _key)
-        public
-        pure
-        returns (uint256 zeroBytesCount_)
-    {
+    function countZeroBytes(bytes32 _key) public pure returns (uint256 zeroBytesCount_) {
         return super._countZeroBytes(_key);
     }
 }
