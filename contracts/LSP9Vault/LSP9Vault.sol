b36202f2be666a161c284ff02027dcee4c344b42
YamenMerhi <yamennmerhi@gmail.com>
2022-05-21 17:09:03 +0300

    refactor: simplify inheritance for LSP9

--- contracts/LSP9Vault/LSP9Vault.sol
+++ contracts/LSP9Vault/LSP9Vault.sol
@@ -19,82 +19,18 @@ import {_INTERFACEID_CLAIM_OWNERSHIP} from "../Utils/IClaimOwnership.sol";
  * @author Fabian Vogelsteller, Yamen Merhi, Jean Cavallera
  * @dev Could be owned by a UniversalProfile and able to register received asset with UniversalReceiverDelegateVault
  */
-contract LSP9Vault is ERC725, LSP9VaultCore {
+contract LSP9Vault is LSP9VaultCore {
     /**
      * @notice Sets the owner of the contract and sets the SupportedStandards:LSP9Vault key
      * @param _newOwner the owner of the contract
      */
-    constructor(address _newOwner) ERC725(_newOwner) {
+    constructor(address _newOwner) {
+        if (_newOwner != owner()) {
+            OwnableUnset.initOwner(_newOwner);
+        }
         // set key SupportedStandards:LSP9Vault
         _setData(_LSP9_SUPPORTED_STANDARDS_KEY, _LSP9_SUPPORTED_STANDARDS_VALUE);
 
         _notifyVaultReceiver(_newOwner);
-    }
-
-    /**
-     * @inheritdoc IERC725Y
-     * @dev Sets data as bytes in the vault storage for a single key.
-     * SHOULD only be callable by the owner of the contract set via ERC173
-     * and the UniversalReceiverDelegate
-     *
-     * Emits a {DataChanged} event.
-     */
-    function setData(bytes32 _key, bytes memory _value) public virtual override onlyAllowed {
-        _setData(_key, _value);
-    }
-
-    /**
-     * @inheritdoc IERC725Y
-     * @dev Sets array of data at multiple given `key`
-     * SHOULD only be callable by the owner of the contract set via ERC173
-     * and the UniversalReceiverDelegate
-     *
-     * Emits a {DataChanged} event.
-     */
-    function setData(bytes32[] memory _keys, bytes[] memory _values)
-        public
-        virtual
-        override
-        onlyAllowed
-    {
-        require(_keys.length == _values.length, "Keys length not equal to values length");
-        for (uint256 i = 0; i < _keys.length; i++) {
-            _setData(_keys[i], _values[i]);
-        }
-    }
-
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(ERC725, LSP9VaultCore)
-        returns (bool)
-    {
-        return
-            interfaceId == _INTERFACEID_LSP9 ||
-            interfaceId == _INTERFACEID_LSP1 ||
-            interfaceId == _INTERFACEID_CLAIM_OWNERSHIP ||
-            super.supportsInterface(interfaceId);
-    }
-
-    /**
-     * @inheritdoc OwnableUnset
-     */
-    function transferOwnership(address newOwner) public virtual override(LSP9VaultCore, OwnableUnset) onlyOwner {
-        ClaimOwnership._transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfer the ownership and notify the vault sender and vault receiver
-     */
-    function claimOwnership() public virtual override {
-        address previousOwner = owner();
-        super.claimOwnership();
-
-        _notifyVaultSender(previousOwner);
-        _notifyVaultReceiver(msg.sender);
     }
 }
