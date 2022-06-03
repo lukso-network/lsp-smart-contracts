69f0e8af001f7625545b8c8aa1782b47ef461663
CJ42 <cavallerajean@gmail.com>
2022-06-03 14:58:11 +0100

    fix: comments

--- contracts/LSP3UniversalProfile/LSP3Constants.sol
+++ contracts/LSP3UniversalProfile/LSP3Constants.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0;
 
-// bytes10(keccak256('SupportedStandard')) + bytes2(0) + bytes20(keccak256('LSP4DigitalAsset'))
+// bytes10(keccak256('SupportedStandard')) + bytes2(0) + bytes20(keccak256('LSP3UniversalProfile'))
 bytes32 constant _LSP3_SUPPORTED_STANDARDS_KEY = 0xeafec4d89fa9619884b60000abe425d64acd861a49b8ddf5c0b6962110481f38;
 
-// bytes4(keccak256('LSP4DigitalAsset'))
+// bytes4(keccak256('LSP3UniversalProfile'))
 bytes constant _LSP3_SUPPORTED_STANDARDS_VALUE = hex"abe425d6";
 
 // bytes32(keccak256("LSP3Profile"))
