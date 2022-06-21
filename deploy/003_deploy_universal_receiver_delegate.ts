7bf668016176bf3fcd1700953e637b7c407924ff
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2022-06-21 08:56:58 +0100

    chore: remove old URD Init deployment script

--- deploy/003_deploy_universal_receiver_delegate.ts
+++ deploy/003_deploy_universal_receiver_delegate.ts
@@ -2,19 +2,19 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
 
-const deployUniversalReceiverDelegate: DeployFunction = async ({
+const deployUniversalReceiverDelegateUP: DeployFunction = async ({
   deployments,
   getNamedAccounts,
 }: HardhatRuntimeEnvironment) => {
   const { deploy } = deployments;
   const { owner } = await getNamedAccounts();
 
-  await deploy("LSP1UniversalReceiverDelegate", {
+  await deploy("LSP1UniversalReceiverDelegateUP", {
     from: owner,
     gasPrice: ethers.BigNumber.from(20_000_000_000), // in wei
     log: true,
   });
 };
 
-export default deployUniversalReceiverDelegate;
-deployUniversalReceiverDelegate.tags = ["LSP1UniversalReceiverDelegate", "standard"];
+export default deployUniversalReceiverDelegateUP;
+deployUniversalReceiverDelegateUP.tags = ["LSP1UniversalReceiverDelegateUP"];
