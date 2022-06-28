4855ba74497c6756523ce86ccbaa23afdbbd9b73
YamenMerhi <yamennmerhi@gmail.com>
2022-06-28 12:32:18 +0300

    feat: add LSP8CompatibleERC721 Mintable contracts

---
+++ contracts/LSP8IdentifiableDigitalAsset/presets/LSP8CompatibleERC721Mintable.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {LSP8CompatibleERC721} from "../extensions/LSP8CompatibleERC721.sol";
+
+contract LSP8CompatibleER7210Mintable is LSP8CompatibleERC721 {
+    /* solhint-disable no-empty-blocks */
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address newOwner_
+    ) LSP8CompatibleERC721(name_, symbol_, newOwner_) {}
+
+    function mint(
+        address to,
+        bytes32 tokenId,
+        bool force,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(to, tokenId, force, data);
+    }
+}
