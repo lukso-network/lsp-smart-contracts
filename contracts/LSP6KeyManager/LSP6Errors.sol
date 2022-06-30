e454b6dd595c4bd7454385b241ccf06a26d3a7a8
CJ42 <31145285+CJ42@users.noreply.github.com>
2022-06-30 15:56:23 +0100

    docs: improve LSP6Errors natspec comments

--- contracts/LSP6KeyManager/LSP6Errors.sol
+++ contracts/LSP6KeyManager/LSP6Errors.sol
@@ -2,42 +2,42 @@
 pragma solidity ^0.8.0;
 
 /**
- * @dev revert when address `from` does not have any permissions set
+ * @dev reverts when address `from` does not have any permissions set
  * on the account linked to this Key Manager
  * @param from the address that does not have permissions
  */
 error NoPermissionsSet(address from);
 
 /**
- * @dev address `from` is not authorised to `permission`
+ * @dev reverts when address `from` is not authorised to perform `permission` on the linked account
  * @param permission permission required
  * @param from address not-authorised
  */
 error NotAuthorised(address from, string permission);
 
 /**
- * @dev address `from` is not authorised to interact with `disallowedAddress` via account
+ * @dev reverts when address `from` is not authorised to interact with `disallowedAddress` via the linked account
  * @param from address making the request
  * @param disallowedAddress address that `from` is not authorised to call
  */
 error NotAllowedAddress(address from, address disallowedAddress);
 
 /**
- * @dev address `from` is not authorised to run `disallowedFunction` via account
+ * @dev reverts when address `from` is not authorised to run `disallowedFunction` via the linked account
  * @param from address making the request
  * @param disallowedFunction bytes4 function selector that `from` is not authorised to run
  */
 error NotAllowedFunction(address from, bytes4 disallowedFunction);
 
 /**
- * @dev address `from` is not authorised to set the key `disallowedKey` on the account
+ * @dev reverts when address `from` is not authorised to set the key `disallowedKey` on the linked account
  * @param from address making the request
  * @param disallowedKey a bytes32 key that `from` is not authorised to set on the ERC725Y storage
  */
 error NotAllowedERC725YKey(address from, bytes32 disallowedKey);
 
 /**
- * @dev the address provided as a target linked to this KeyManager is invalid
+ * @dev reverts when the address provided as a target (= account) linked to this KeyManager is invalid
  *      e.g. address(0)
  */
 error InvalidLSP6Target();
