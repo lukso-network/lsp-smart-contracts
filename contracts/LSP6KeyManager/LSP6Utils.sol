291e6bfb41e1bebd6244168b10a4e603f4d26714
YamenMerhi <yamennmerhi@gmail.com>
2022-07-20 13:56:25 +0300

    chore: add setup permission function in LSP6Utils

--- contracts/LSP6KeyManager/LSP6Utils.sol
+++ contracts/LSP6KeyManager/LSP6Utils.sol
@@ -53,44 +53,6 @@ library LSP6Utils {
             );
     }
 
-    function setupPermissions(
-        IERC725Y _account,
-        address _address,
-        bytes memory permissions
-    ) internal view returns (bytes32[] memory keys, bytes[] memory values) {
-        keys = new bytes32[](3);
-        values = new bytes[](3);
-
-        bytes memory rawArrayLength = ERC725Utils.getDataSingle(
-            _account,
-            _LSP6_ADDRESS_PERMISSIONS_ARRAY_KEY
-        );
-
-        keys[0] = _LSP6_ADDRESS_PERMISSIONS_ARRAY_KEY;
-        keys[2] = _LSP6_ADDRESS_PERMISSIONS_MAP_KEY_PREFIX.generateBytes20MappingWithGroupingKey(
-            bytes20(_address)
-        );
-
-        values[1] = UtilsLib.addressToBytes(_address);
-        values[2] = permissions;
-
-        if (bytes32(rawArrayLength) == bytes32(0)) {
-            keys[1] = LSP2Utils.generateArrayKeyAtIndex(_LSP6_ADDRESS_PERMISSIONS_ARRAY_KEY, 0);
-
-            values[0] = abi.encodePacked(uint256(1));
-        } else if (rawArrayLength.length == 32) {
-            uint256 arrayLength = abi.decode(rawArrayLength, (uint256));
-            uint256 newArrayLength = arrayLength + 1;
-
-            keys[1] = LSP2Utils.generateArrayKeyAtIndex(
-                _LSP6_ADDRESS_PERMISSIONS_ARRAY_KEY,
-                arrayLength
-            );
-
-            values[0] = UtilsLib.uint256ToBytes(newArrayLength);
-        }
-    }
-
     function getAllowedStandardsFor(IERC725Y target, address caller)
         internal
         view
@@ -141,5 +103,32 @@ library LSP6Utils {
     ) internal returns (bytes memory result) {
         bytes memory payload = abi.encodeWithSelector(hex"14a6e293", keys, values);
         result = ILSP6KeyManager(keyManagerAddress).execute(payload);
+    }
+
+    function setupPermissions(
+        IERC725Y _account,
+        address _address,
+        bytes memory permissions
+    ) internal view returns (bytes32[] memory keys, bytes[] memory values) {
+        keys = new bytes32[](3);
+        values = new bytes[](3);
+
+        uint256 arrayLength = uint256(bytes32(_account.getData(_LSP6KEY_ADDRESSPERMISSIONS_ARRAY)));
+        uint256 newArrayLength = arrayLength + 1;
+
+        keys[0] = _LSP6KEY_ADDRESSPERMISSIONS_ARRAY;
+        values[0] = abi.encodePacked(newArrayLength);
+
+        keys[1] = LSP2Utils.generateArrayElementKeyAtIndex(
+            _LSP6KEY_ADDRESSPERMISSIONS_ARRAY,
+            arrayLength
+        );
+        values[1] = abi.encodePacked(_address);
+
+        keys[2] = LSP2Utils.generateMappingWithGroupingKey(
+            _LSP6KEY_ADDRESSPERMISSIONS_PERMISSIONS_PREFIX,
+            bytes20(_address)
+        );
+        values[2] = permissions;
     }
 }
