e4d7c757387b003c9591efe72822f736fc527e10
Maxime <maximeviard@hotmail.fr>
2022-08-15 16:19:59 +0300

    fix: Replace bytes8 comment by bytes10

--- contracts/LSP8IdentifiableDigitalAsset/LSP8Constants.sol
+++ contracts/LSP8IdentifiableDigitalAsset/LSP8Constants.sol
@@ -6,10 +6,10 @@ bytes4 constant _INTERFACEID_LSP8 = 0x49399145;
 
 // --- ERC725Y Keys
 
-// bytes8(keccak256('LSP8MetadataAddress')) + bytes2(0)
+// bytes10(keccak256('LSP8MetadataAddress')) + bytes2(0)
 bytes12 constant _LSP8_METADATA_ADDRESS_KEY_PREFIX = 0x73dcc7c3c4096cdc7f8a0000;
 
-// bytes8(keccak256('LSP8MetadataJSON')) + bytes2(0)
+// bytes10(keccak256('LSP8MetadataJSON')) + bytes2(0)
 bytes12 constant _LSP8_METADATA_JSON_KEY_PREFIX = 0x9a26b4060ae7f7d5e3cd0000;
 
 // --- Token Hooks
