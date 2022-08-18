318258a9d7189b58b6cfbfa7d689185e602d6cb9
b00ste <b00ste.lyx@gmail.com>
2022-08-18 08:57:28 +0000

    fix: Adding `_disableInitializers` to LSP7 & LSP8 mintable presets contracts

--- contracts/LSP7DigitalAsset/presets/LSP7CompatibleERC20MintableInit.sol
+++ contracts/LSP7DigitalAsset/presets/LSP7CompatibleERC20MintableInit.sol
@@ -8,6 +8,13 @@ import {
 
 contract LSP7CompatibleERC20MintableInit is LSP7CompatibleERC20MintableInitAbstract {
     /**
+     * @dev initialize (= lock) base implementation contract on deployment
+     */
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
      * @notice Sets the name, the symbol and the owner of the token
      * @param name_ The name of the token
      * @param symbol_ The symbol of the token
