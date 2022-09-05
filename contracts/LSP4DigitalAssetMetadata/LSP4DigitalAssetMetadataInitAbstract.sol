08d64d58c6d00aabd1103e4aed093d6ee3813099
CJ42 <31145285+CJ42@users.noreply.github.com>
2022-09-05 09:40:55 +0100

    refactor!: override `_setData(...)` to emit DataChanged event with value up to 256 bytes

--- contracts/LSP4DigitalAssetMetadata/LSP4DigitalAssetMetadataInitAbstract.sol
+++ contracts/LSP4DigitalAssetMetadata/LSP4DigitalAssetMetadataInitAbstract.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.0;
 import {ERC725YInitAbstract} from "@erc725/smart-contracts/contracts/ERC725YInitAbstract.sol";
 import {ERC725YCore} from "@erc725/smart-contracts/contracts/ERC725YCore.sol";
 
+// libraries
+import {BytesLib} from "solidity-bytes-utils/contracts/BytesLib.sol";
+
 // constants
 import "./LSP4Constants.sol";
 
@@ -22,7 +25,6 @@ abstract contract LSP4DigitalAssetMetadataInitAbstract is ERC725YInitAbstract {
         string memory symbol_,
         address newOwner_
     ) internal virtual onlyInitializing {
-        require(newOwner_ != address(0), "LSP4: new owner cannot be the zero address");
         ERC725YInitAbstract._initialize(newOwner_);
 
         // set SupportedStandards:LSP4DigitalAsset
@@ -33,16 +35,22 @@ abstract contract LSP4DigitalAssetMetadataInitAbstract is ERC725YInitAbstract {
     }
 
     /**
-     * @dev the ERC725Y data keys `LSP4TokenName` and `LSP4TokenSymbol` cannot be changed via this function
-     *      once the digital asset contract has been deployed
+     * @dev the ERC725Y data keys `LSP4TokenName` and `LSP4TokenSymbol` cannot be changed
+     *      via this function once the digital asset contract has been deployed.
+     *
+     * @dev SAVE GAS by emitting the DataChanged event with only the first 256 bytes of dataValue
      */
-    function _setData(bytes32 key, bytes memory value) internal virtual override {
-        if (key == _LSP4_TOKEN_NAME_KEY) {
+    function _setData(bytes32 dataKey, bytes memory dataValue) internal virtual override {
+        if (dataKey == _LSP4_TOKEN_NAME_KEY) {
             revert LSP4TokenNameNotEditable();
-        } else if (key == _LSP4_TOKEN_SYMBOL_KEY) {
+        } else if (dataKey == _LSP4_TOKEN_SYMBOL_KEY) {
             revert LSP4TokenSymbolNotEditable();
         } else {
-            super._setData(key, value);
+            store[dataKey] = dataValue;
+            emit DataChanged(
+                dataKey,
+                dataValue.length <= 256 ? dataValue : BytesLib.slice(dataValue, 0, 256)
+            );
         }
     }
 }
