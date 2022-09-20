0dafe3f0af5f8970c09526abf757226c8bd1b6e2
YamenMerhi <yamennmerhi@gmail.com>
2022-09-20 13:48:30 +0300

    refactor: refactor LSP1Utils

---
+++ contracts/LSP1UniversalReceiver/LSP1Errors.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+/**
+ * @dev reverts when EOA calls the `universalReceiver(..)` function with an asset/vault typeId
+ * @param caller The address of the EOA
+ */
+error CannotRegisterEOAsAsAssets(address caller);
+
+/**
+ * @dev reverts when the account calling the UniversalReceiverDelegate is not the same account
+ * linked as target in the KeyManager -- Security Check
+ * @param account The address of the account implementing the `universalReceiver(..)` function
+ * @param target The address of the target linked to the KeyManager
+ */
+error MsgSenderIsNotTheLinkedTarget(address account, address target);
--- contracts/LSP1UniversalReceiver/LSP1Utils.sol
+++ contracts/LSP1UniversalReceiver/LSP1Utils.sol
@@ -2,61 +2,41 @@
 pragma solidity ^0.8.0;
 
 // constants
-import {
-    _LSP5_RECEIVED_ASSETS_ARRAY_KEY,
-    _LSP5_RECEIVED_ASSETS_MAP_KEY_PREFIX
-} from "../LSP5ReceivedAssets/LSP5Constants.sol";
-import {
-    _INTERFACEID_LSP7,
-    _TYPEID_LSP7_TOKENSSENDER,
-    _TYPEID_LSP7_TOKENSRECIPIENT
-} from "../LSP7DigitalAsset/LSP7Constants.sol";
-import {
-    _INTERFACEID_LSP8,
-    _TYPEID_LSP8_TOKENSRECIPIENT,
-    _TYPEID_LSP8_TOKENSSENDER
-} from "../LSP8IdentifiableDigitalAsset/LSP8Constants.sol";
-import {_INTERFACEID_LSP9, _TYPEID_LSP9_VAULTRECIPIENT} from "../LSP9Vault/LSP9Constants.sol";
-import {
-    _LSP10_VAULTS_ARRAY_KEY,
-    _LSP10_VAULTS_MAP_KEY_PREFIX
-} from "../LSP10ReceivedVaults/LSP10Constants.sol";
+import "../LSP5ReceivedAssets/LSP5Constants.sol";
+import "../LSP7DigitalAsset/LSP7Constants.sol";
+import "../LSP8IdentifiableDigitalAsset/LSP8Constants.sol";
+import "../LSP9Vault/LSP9Constants.sol";
+import "../LSP10ReceivedVaults/LSP10Constants.sol";
 
 library LSP1Utils {
     /**
      * @dev Gets all the transfer details depending on the `typeId`
+     * @param typeId A unique identifier for a specific action
      */
     function getTransferDetails(bytes32 typeId)
         internal
         pure
         returns (
-            bool isReceiving,
-            bytes32 arrayKey,
+            bool invalid,
             bytes10 mapPrefix,
-            bytes4 interfaceId
+            bytes4 interfaceId,
+            bool isReceiving
         )
     {
-        if (
-            typeId == _TYPEID_LSP7_TOKENSSENDER ||
-            typeId == _TYPEID_LSP7_TOKENSRECIPIENT ||
-            typeId == _TYPEID_LSP8_TOKENSRECIPIENT ||
-            typeId == _TYPEID_LSP8_TOKENSSENDER
-        ) {
-            arrayKey = _LSP5_RECEIVED_ASSETS_ARRAY_KEY;
+        if (typeId == _TYPEID_LSP7_TOKENSSENDER || typeId == _TYPEID_LSP7_TOKENSRECIPIENT) {
             mapPrefix = _LSP5_RECEIVED_ASSETS_MAP_KEY_PREFIX;
-            interfaceId = typeId == _TYPEID_LSP7_TOKENSSENDER ||
-                typeId == _TYPEID_LSP7_TOKENSRECIPIENT
-                ? _INTERFACEID_LSP7
-                : _INTERFACEID_LSP8;
-            isReceiving = typeId == _TYPEID_LSP7_TOKENSRECIPIENT ||
-                typeId == _TYPEID_LSP8_TOKENSRECIPIENT
-                ? true
-                : false;
-        } else {
-            arrayKey = _LSP10_VAULTS_ARRAY_KEY;
+            interfaceId = _INTERFACEID_LSP7;
+            isReceiving = typeId == _TYPEID_LSP7_TOKENSRECIPIENT ? true : false;
+        } else if (typeId == _TYPEID_LSP8_TOKENSSENDER || typeId == _TYPEID_LSP8_TOKENSRECIPIENT) {
+            mapPrefix = _LSP5_RECEIVED_ASSETS_MAP_KEY_PREFIX;
+            interfaceId = _INTERFACEID_LSP8;
+            isReceiving = typeId == _TYPEID_LSP8_TOKENSRECIPIENT ? true : false;
+        } else if (typeId == _TYPEID_LSP9_VAULTSENDER || typeId == _TYPEID_LSP9_VAULTRECIPIENT) {
             mapPrefix = _LSP10_VAULTS_MAP_KEY_PREFIX;
             interfaceId = _INTERFACEID_LSP9;
             isReceiving = typeId == _TYPEID_LSP9_VAULTRECIPIENT ? true : false;
+        } else {
+            invalid = true;
         }
     }
 }
