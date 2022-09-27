9e352575a31e888133e1eea65b8bb70d6f269a61
YamenMerhi <yamennmerhi@gmail.com>
2022-09-27 16:03:15 +0300

    test: add ABIEncoder tests

---
+++ contracts/Helpers/ABIEncoder.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+contract ABIEncoder {
+    function encode(bytes memory a, bytes memory b)
+        public
+        returns (bytes memory c, uint256 gasUsed)
+    {
+        uint256 gasUsed1 = gasleft();
+        c = abi.encode(a, b);
+        uint256 gasUsed2 = gasleft();
+        gasUsed = gasUsed1 - gasUsed2;
+    }
+
+    function decode(bytes memory c) public pure returns (bytes memory a, bytes memory b) {
+        (a, b) = abi.decode(c, (bytes, bytes));
+    }
+}
