57c4a26975628b0c4ecd85de302c00f5b88b4350
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2022-10-25 11:23:24 +0100

    fix: [WP-M9] LSP-2: `LSP2Utils.isEncodedArray()` Incomplete implementation (#342)
    * fix: error when providing 32 bytes value to `LSP2Utils.isEncodedArray(...)`
    
    * test: add test when providing a `bytes32` encoded value
    
    * refactor: remove unecessary initial check for `nbOfBytes`

--- tests/LSP2ERC725YJSONSchema/LSP2UtilsLibrary.test.ts
+++ tests/LSP2ERC725YJSONSchema/LSP2UtilsLibrary.test.ts
@@ -6,6 +6,8 @@ import {
   LSP2UtilsLibraryTester__factory,
 } from "../../types";
 
+import { abiCoder } from "../utils/helpers";
+
 describe("LSP2Utils", () => {
   let accounts: SignerWithAddress[];
   let lsp2Utils: LSP2UtilsLibraryTester;
@@ -70,6 +72,31 @@ describe("LSP2Utils", () => {
     });
 
     describe("testing various non-zero bytes input", () => {
+      describe("when 32 bytes", () => {
+        it("should return false (`uint256` data = 32)", async () => {
+          const data = abiCoder.encode(["uint256"], [32]);
+          const result = await lsp2Utils.isEncodedArray(data);
+          expect(result).to.be.false;
+        });
+
+        it("should return false (`uint256` data = 12345)", async () => {
+          const data = abiCoder.encode(["uint256"], [12345]);
+          const result = await lsp2Utils.isEncodedArray(data);
+          expect(result).to.be.false;
+        });
+
+        it("should return false (`bytes32` data = 0xcafecafecafecafe...)", async () => {
+          const data = abiCoder.encode(
+            ["bytes32"],
+            [
+              "0xcafecafecafecafecafecafecafecafecafecafecafecafecafecafecafecafe",
+            ]
+          );
+          const result = await lsp2Utils.isEncodedArray(data);
+          expect(result).to.be.false;
+        });
+      });
+
       describe("when less than 32 bytes", () => {
         it("should return false with 4x random bytes", async () => {
           const data = "0xaabbccdd";
