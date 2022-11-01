0d32a8d086cceb40a46c4813f22db4c9a2ce4368
Max <maximeviard@hotmail.fr>
2022-11-01 16:50:25 +0100

    refactor: fix error in LSP2Utils

--- contracts/LSP2ERC725YJSONSchema/LSP2Utils.sol
+++ contracts/LSP2ERC725YJSONSchema/LSP2Utils.sol
@@ -266,18 +266,6 @@ library LSP2Utils {
     }
 
     /**
-     * @dev verify that `data` a valid CompactFixedSizeBytesArray
-     * @param compactFixedSizeBytesArray The value that is to be verified
-     * @param fixedSize The size of each element in bytes
-     */
-    function checkValidCompactFixedSizeBytesArray(
-        bytes memory compactFixedSizeBytesArray,
-        uint256 fixedSize
-    ) public pure returns (bool isValid) {
-        if (compactFixedSizeBytesArray.length % (fixedSize + 1) == 0) isValid = true;
-    }
-
-    /**
      * @dev Will return unchecked incremented uint256
      *      can be used to save gas when iterating over loops
      */
@@ -285,5 +273,17 @@ library LSP2Utils {
         unchecked {
             return i + 1;
         }
+    }
+
+    /**
+     * @dev verify that `data` a valid CompactFixedSizeBytesArray
+     * @param compactFixedSizeBytesArray The value that is to be verified
+     * @param fixedSize The size of each element in bytes
+     */
+    function checkValidCompactFixedSizeBytesArray(
+        bytes memory compactFixedSizeBytesArray,
+        uint256 fixedSize
+    ) internal pure returns (bool isValid) {
+        if (compactFixedSizeBytesArray.length % (fixedSize + 1) == 0) isValid = true;
     }
 }
