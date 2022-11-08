9f91c83ae079cae0904f5cb1bbe2c2ba70fa87a5
CJ42 <cavallerajean@gmail.com>
2022-11-08 15:13:47 +0000

    refactor!: mark `initialize(...)` function as `external`

--- contracts/LSP8IdentifiableDigitalAsset/presets/LSP8CompatibleERC721MintableInit.sol
+++ contracts/LSP8IdentifiableDigitalAsset/presets/LSP8CompatibleERC721MintableInit.sol
@@ -13,7 +13,7 @@ contract LSP8CompatibleERC721MintableInit is LSP8CompatibleERC721MintableInitAbs
     constructor() {
         _disableInitializers();
     }
-    
+
     /**
      * @notice Sets the name, the symbol and the owner of the token
      * @param name_ The name of the token
@@ -24,7 +24,7 @@ contract LSP8CompatibleERC721MintableInit is LSP8CompatibleERC721MintableInitAbs
         string memory name_,
         string memory symbol_,
         address newOwner_
-    ) public virtual initializer {
+    ) external virtual initializer {
         LSP8CompatibleERC721MintableInitAbstract._initialize(name_, symbol_, newOwner_);
     }
 }
