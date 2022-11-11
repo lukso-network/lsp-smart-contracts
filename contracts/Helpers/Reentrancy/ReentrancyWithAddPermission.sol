42343c8694420d7a2c76c38b71bf2bfd056fc8c4
Daniel Afteni <b00ste.lyx@gmail.com>
2022-11-11 13:04:59 +0200

    test: finalize the contracts for reentrancy test cases

--- contracts/Helpers/Reentrancy/ReentrancyWithAddPermission.sol
+++ contracts/Helpers/Reentrancy/ReentrancyWithAddPermission.sol
@@ -2,28 +2,23 @@
 pragma solidity ^0.8.0;
 
 // interfaces
-import {IERC725Y} from "@erc725/smart-contracts/contracts/interfaces/IERC725Y.sol";
 import {ILSP6KeyManager} from "../../LSP6KeyManager/ILSP6KeyManager.sol";
 import {LSP14Ownable2Step} from "../../LSP14Ownable2Step/LSP14Ownable2Step.sol";
 
 // constants
-import {
-    _LSP6KEY_ADDRESSPERMISSIONS_ARRAY,
-    _LSP6KEY_ADDRESSPERMISSIONS_ARRAY_PREFIX,
-    _LSP6KEY_ADDRESSPERMISSIONS_PERMISSIONS_PREFIX
-} from "../../LSP6KeyManager/LSP6Constants.sol";
+import "../../LSP6KeyManager/LSP6Constants.sol";
 
 contract ReentrancyWithAddPermission {
     function universalReceiverDelegate(
         address sender,
         uint256 value, // solhint-disable no-unused-vars
         bytes32 typeId, // solhint-disable no-unused-vars
-        bytes memory data
+        bytes memory data // bytes20(address(controller))
     ) public virtual returns (bytes memory result) {
         // solhint-disable no-unused-vars
         address keyManager = LSP14Ownable2Step(sender).owner();
 
-        bytes memory setDataPayload = abi.encodeWithSignature(
+        bytes memory addPermissionPayload = abi.encodeWithSignature(
             "setData(bytes32,bytes)",
             bytes32(
                 bytes.concat(
@@ -35,6 +30,6 @@ contract ReentrancyWithAddPermission {
             bytes.concat(bytes32(uint256(16)))
         );
 
-        ILSP6KeyManager(keyManager).execute(setDataPayload);
+        ILSP6KeyManager(keyManager).execute(addPermissionPayload);
     }
 }
