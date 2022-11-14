d56ea37d352c9ec719be9c44aaf5584c9ee2f92f
Daniel Afteni <b00ste.lyx@gmail.com>
2022-11-14 19:02:50 +0200

    test: make sure that an lsp0 is not registered as areceived vault

--- contracts/LSP1UniversalReceiver/LSP1UniversalReceiverDelegateUP/LSP1UniversalReceiverDelegateUP.sol
+++ contracts/LSP1UniversalReceiver/LSP1UniversalReceiverDelegateUP/LSP1UniversalReceiverDelegateUP.sol
@@ -64,6 +64,7 @@ contract LSP1UniversalReceiverDelegateUP is ERC165, ILSP1UniversalReceiverDelega
         (address keyManager, bool ownerIsKeyManager) = _validateCallerViaKeyManager();
         if (!ownerIsKeyManager) return "LSP1: account owner is not a LSP6KeyManager";
 
+        // if receiving ownership of an LSP0, do not register it as a received vault
         if (mapPrefix == _LSP10_VAULTS_MAP_KEY_PREFIX) {
             if (notifier.code.length != 0) {
                 if (!notifier.supportsERC165Interface(_INTERFACEID_LSP9))
