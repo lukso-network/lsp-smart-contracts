eca31da50c11c4b5c90972f496c6c273ec28315a
Lucas MT <lucas.tabajara@runtimeverification.com>
2022-12-07 14:19:10 -0600

    build: Update the compiler version to 0.8.13

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -94,7 +94,7 @@ const config: HardhatUserConfig = {
     showMethodSig: true,
   },
   solidity: {
-    version: "0.8.10",
+    version: "0.8.13",
     settings: {
       optimizer: {
         enabled: true,
