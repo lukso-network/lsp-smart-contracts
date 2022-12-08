e7a07d675619f2e35b9bc92c9b43f5d06ff9acd2
Yamen Merhi <yamennmerhi@gmail.com>
2022-12-08 13:51:14 +0200

    feat!: adjust supportsInterface in LSP0/9 to support Interfaces of ERC165Extension (#392)
    * feat: implement the support for InterfaceId in ERC165Extension
    
    * test: add tests to ERC165 extension
    
    * chore: remove non needed imports
    
    * chore: improve tests and natspec
    
    * chore: simplify LSP17 code
    
    * test: change beforeEach to before
    
    * chore: opitimize code

---
+++ contracts/Helpers/FallbackExtensions/ERC165Extension.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.4;
+
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+/**
+ * @dev This contract is used only for testing purposes
+ */
+contract ERC165Extension is IERC165 {
+    bytes4 private constant _RANDOM_INTERFACE_ID = 0xaabbccdd;
+
+    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
+        return interfaceId == _RANDOM_INTERFACE_ID;
+    }
+}
