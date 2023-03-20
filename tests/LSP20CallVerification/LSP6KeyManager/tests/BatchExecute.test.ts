3411dd5dabeac735f8b1c0f40f44f3da63bb5b03
Yamen Merhi <yamennmerhi@gmail.com>
2023-03-20 11:10:20 +0200

    fix: disallow calling LSP20 function on LSP6 through execute (#508)
    * fix: disallow calling lsp20 functions from execute in LSP6
    
    * test: add necessary tests
    
    * test: resolve failing tests
    
    * test: remove .only

--- tests/LSP20CallVerification/LSP6KeyManager/tests/BatchExecute.test.ts
+++ tests/LSP20CallVerification/LSP6KeyManager/tests/BatchExecute.test.ts
@@ -496,7 +496,7 @@ export const shouldBehaveLikeBatchExecute = (
         });
 
         describe("if specifying some value for each values[index]", () => {
-          it("should revert LSP6 `_executePayload` error since `setData(...)` is not payable", async () => {
+          it("should revert with Key Manager error `CannotSendValueToSetData` when sending value while setting data", async () => {
             const amountToFund = ethers.utils.parseEther("2");
 
             const dataKeys = [
@@ -531,19 +531,10 @@ export const shouldBehaveLikeBatchExecute = (
                   [firstSetDataPayload, secondSetDataPayload],
                   { value: amountToFund }
                 )
-            ).to.be.revertedWith("LSP6: failed executing payload");
-
-            const keyManagerBalanceAfter = await ethers.provider.getBalance(
-              context.keyManager.address
+            ).to.be.revertedWithCustomError(
+              context.keyManager,
+              "CannotSendValueToSetData"
             );
-
-            expect(keyManagerBalanceAfter).to.equal(keyManagerBalanceBefore);
-
-            // the Key Manager must not hold any funds and must always forward any funds sent to it.
-            // it's balance must always be 0 after any execution
-            expect(
-              await provider.getBalance(context.keyManager.address)
-            ).to.equal(0);
           });
         });
       });
@@ -633,7 +624,10 @@ export const shouldBehaveLikeBatchExecute = (
                 ["execute(uint256[],bytes[])"](msgValues, payloads, {
                   value: totalValues,
                 })
-            ).to.be.revertedWith("LSP6: failed executing payload");
+            ).to.be.revertedWithCustomError(
+              context.keyManager,
+              "CannotSendValueToSetData"
+            );
           });
         });
       });
