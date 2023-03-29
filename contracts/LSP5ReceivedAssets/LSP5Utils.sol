9e510a6f0c60b12180dd8620481316bf1c994d1c
b00ste.lyx <62855857+b00ste@users.noreply.github.com>
2023-03-29 14:32:27 +0300

    refactor: do not revert when LSP5/LSP10 map data is corrupted (#527)
    * refactor: return when asset map data is corrupted
    
    * test: add tests for asset corrupted data
    
    * refactor: add checks for handling corrupted data
    
    Co-authored-by: Jean Cvllr <CJ42@users.noreply.github.com>
    
    * test: add more tests for edge cases
    
    Co-authored-by: Jean Cvllr <31145285+CJ42@users.noreply.github.com>
    
    * chore: add more natspec for context
    
    * refactor: optimise gas
    
    * test: change `beforeEach` to `before`
    
    * test: refactor tests from `beforeEach` to `before`
    
    * refactor: remove external call check
    
    * chore: update natspec
    
    ---------
    
    Co-authored-by: Jean Cvllr <CJ42@users.noreply.github.com>
    Co-authored-by: Jean Cvllr <31145285+CJ42@users.noreply.github.com>

--- contracts/LSP5ReceivedAssets/LSP5Utils.sol
+++ contracts/LSP5ReceivedAssets/LSP5Utils.sol
@@ -149,7 +149,7 @@ library LSP5Utils {
 
             // Swapping last element in ArrayKey with the element in ArrayKey to remove || {Swap and pop} method;
             // check https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/structs/EnumerableSet.sol#L80
-        } else {
+        } else if (index < newArrayLength) {
             /**
              * We will be updating/removing 5 keys:
              * - Keys[0]: [Update] The arrayLengthKey to contain the new number of the received assets
@@ -196,6 +196,9 @@ library LSP5Utils {
 
             keys[4] = lastAssetInArrayMapKey;
             values[4] = bytes.concat(interfaceID, bytes16(index));
+        } else {
+            // If index is bigger than the array length, out of bounds
+            return (keys, values);
         }
     }
 
