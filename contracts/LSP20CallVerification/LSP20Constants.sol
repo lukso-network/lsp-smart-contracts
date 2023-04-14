bacfc8026c3ef68bb694995b3cab5c21521eda1c
b00ste <b00ste.lyx@gmail.com>
2023-04-14 19:35:17 +0300

    chore: fix typo

--- contracts/LSP20CallVerification/LSP20Constants.sol
+++ contracts/LSP20CallVerification/LSP20Constants.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-// bytes4(keccack256("LSP20CallVerification"))
+// bytes4(keccak256("LSP20CallVerification"))
 bytes4 constant _INTERFACEID_LSP20_CALL_VERIFICATION = 0x1a0eb6a5;
 
 // Xor of `lsp20VerifyCall(address,uint256,bytes)` and `lsp20VerifyCallResult(bytes32,bytes)`
