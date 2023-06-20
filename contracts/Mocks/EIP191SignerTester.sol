19a6de9fcb01858127146db2e408eba965cc0d8f
CJ42 <cavallerajean@gmail.com>
2023-06-20 17:02:15 +0100

    refactor!: remove `EIP191Signer` library to use function from OZ library

--- contracts/Mocks/EIP191SignerTester.sol
+++ contracts/Mocks/EIP191SignerTester.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.4;
 
-import {EIP191Signer} from "../Custom/EIP191Signer.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract EIP191SignerTester {
     function toDataWithIntendedValidator(
         address validator,
         bytes memory dataToSign
     ) public pure returns (bytes32) {
-        return EIP191Signer.toDataWithIntendedValidator(validator, dataToSign);
+        return ECDSA.toDataWithIntendedValidatorHash(validator, dataToSign);
     }
 }
