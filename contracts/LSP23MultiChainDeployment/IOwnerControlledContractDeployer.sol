01fd82038811ba65798f3ff8fe8273fd191dcb38
Skima Harvey <64636974+skimaharvey@users.noreply.github.com>
2023-08-04 13:14:12 +0200

    feat: create LSP23MultiChainDeployment (#649)
    * feat: create LSP23MultiChainDeployment
    
    * refactor: improve LSP23 readability (#650)
    
    * refactor: improve LSP23 readability
    
    * tests: create helper to calculate deployed addresses
    
    * tests: add test for lsp23
    
    * refactor: add suggested changes & surther simplify
    
    * refactor: remove event split, restore the initial events.
    
    * refactor: fix generation of salt
    
    * refactor: make salt generation functions virtual
    
    * chore: fix typo
    
    * refactor: move `emit` before external call to `IPostDeploymentModule`
    
    * chore: add suggested changes
    
    * docs: improve Natspec as suggetsed
    
    * docs: fix Natspec typo
    
    * chore: remove unrelated file
    
    ---------
    
    Co-authored-by: maxvia87 <maximeviard@hotmail.fr>
    
    ---------
    
    Co-authored-by: b00ste.lyx <62855857+b00ste@users.noreply.github.com>
    Co-authored-by: Jean Cvllr <31145285+CJ42@users.noreply.github.com>

---
+++ contracts/LSP23MultiChainDeployment/IOwnerControlledContractDeployer.sol
@@ -0,0 +1,174 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IOwnerControlledContractDeployer {
+    event DeployedContracts(
+        address indexed controlledContract,
+        address indexed ownerContract,
+        ControlledContractDeployment controlledContractDeployment,
+        OwnerContractDeployment ownerContractDeployment,
+        address postDeploymentModule,
+        bytes postDeploymentModuleCalldata
+    );
+
+    event DeployedERC1167Proxies(
+        address indexed controlledContract,
+        address indexed ownerContract,
+        ControlledContractDeploymentInit controlledContractDeploymentInit,
+        OwnerContractDeploymentInit ownerContractDeploymentInit,
+        address postDeploymentModule,
+        bytes postDeploymentModuleCalldata
+    );
+
+    /**
+     * @param salt A unique value used to ensure each created proxies are unique. (Can be used to deploy the contract at a desired address.)
+     * @param fundingAmount The value to be sent with the deployment transaction.
+     * @param creationBytecode The bytecode of the contract with the constructor params.
+     */
+    struct ControlledContractDeployment {
+        bytes32 salt;
+        uint256 fundingAmount;
+        bytes creationBytecode;
+    }
+
+    /**
+     * @param fundingAmount The value to be sent with the deployment transaction.
+     * @param creationBytecode The constructor + runtime bytecode (without the controlled contract's address as param)
+     * @param addControlledContractAddress If set to `true`, this will append the controlled contract's address + the `extraConstructorParams` to the `creationBytecode`.
+     * @param extraConstructorParams Params to be appended to the `creationBytecode` (after the controlled contract address) if `addControlledContractAddress` is set to `true`.
+     */
+    struct OwnerContractDeployment {
+        uint256 fundingAmount;
+        bytes creationBytecode;
+        bool addControlledContractAddress;
+        bytes extraConstructorParams;
+    }
+
+    /**
+     * @param salt A unique value used to ensure each created proxies are unique. (Can be used to deploy the contract at a desired address.)
+     * @param fundingAmount The value to be sent with the deployment transaction.
+     * @param implementationContract The address of the contract that will be used as a base contract for the proxy.
+     * @param initializationCalldata The calldata used to initialise the contract. (initialization should be similar to a constructor in a normal contract.)
+     */
+    struct ControlledContractDeploymentInit {
+        bytes32 salt;
+        uint256 fundingAmount;
+        address implementationContract;
+        bytes initializationCalldata;
+    }
+
+    /**
+     * @param fundingAmount The value to be sent with the deployment transaction.
+     * @param implementationContract The address of the contract that will be used as a base contract for the proxy.
+     * @param initializationCalldata The first part of the initialisation calldata, everything before the controlled contract address.
+     * @param addControlledContractAddress If set to `true`, this will append the controlled contract's address + the `extraInitializationParams` to the `initializationCalldata`.
+     * @param extraInitializationParams Params to be appended to the `initializationCalldata` (after the controlled contract address) if `addControlledContractAddress` is set to `true`
+     */
+    struct OwnerContractDeploymentInit {
+        uint256 fundingAmount;
+        address implementationContract;
+        bytes initializationCalldata;
+        bool addControlledContractAddress;
+        bytes extraInitializationParams;
+    }
+
+    /**
+     * @dev Deploys a contract and its owner contract.
+     * @notice Contracts deployed. Contract Address: `controlledContractAddress`. Owner Contract Address: `ownerContractAddress`
+     *
+     * @param controlledContractDeployment Contains the needed parameter to deploy a contract. (`salt`, `fundingAmount`, `creationBytecode`)
+     * @param ownerContractDeployment Contains the needed parameter to deploy the owner contract. (`fundingAmount`, `creationBytecode`, `addControlledContractAddress`, `extraConstructorParams`)
+     * @param postDeploymentModule The module to be executed after deployment
+     * @param postDeploymentModuleCalldata The data to be passed to the post deployment module
+     *
+     * @return controlledContractAddress The address of the deployed controlled contract.
+     * @return ownerContractAddress The address of the deployed owner contract.
+     */
+    function deployContracts(
+        ControlledContractDeployment calldata controlledContractDeployment,
+        OwnerContractDeployment calldata ownerContractDeployment,
+        address postDeploymentModule,
+        bytes calldata postDeploymentModuleCalldata
+    )
+        external
+        payable
+        returns (
+            address controlledContractAddress,
+            address ownerContractAddress
+        );
+
+    /**
+     * @dev Deploys proxies of a contract and its owner contract
+     * @notice Contract proxies deployed. Contract Proxy Address: `controlledContractAddress`. Owner Contract Proxy Address: `ownerContractAddress`
+     *
+     * @param controlledContractDeploymentInit Contains the needed parameter to deploy a proxy contract. (`salt`, `fundingAmount`, `implementationContract`, `initializationCalldata`)
+     * @param ownerContractDeploymentInit Contains the needed parameter to deploy the owner proxy contract. (`fundingAmount`, `implementationContract`, `addControlledContractAddress`, `initializationCalldata`, `extraInitializationParams`)
+     * @param postDeploymentModule The module to be executed after deployment.
+     * @param postDeploymentModuleCalldata The data to be passed to the post deployment module.
+     *
+     * @return controlledContractAddress The address of the deployed controlled contract proxy
+     * @return ownerContractAddress The address of the deployed owner contract proxy
+     */
+    function deployERC1167Proxies(
+        ControlledContractDeploymentInit
+            calldata controlledContractDeploymentInit,
+        OwnerContractDeploymentInit calldata ownerContractDeploymentInit,
+        address postDeploymentModule,
+        bytes calldata postDeploymentModuleCalldata
+    )
+        external
+        payable
+        returns (
+            address controlledContractAddress,
+            address ownerContractAddress
+        );
+
+    /**
+     * @dev Computes the addresses of the controlled and owner contracts to be created
+     *
+     * @param controlledContractDeployment Contains the needed parameter to deploy a contract. (`salt`, `fundingAmount`, `creationBytecode`)
+     * @param ownerContractDeployment Contains the needed parameter to deploy the owner contract. (`fundingAmount`, `creationBytecode`, `addControlledContractAddress`, `extraConstructorParams`)
+     * @param postDeploymentModule The module to be executed after deployment
+     * @param postDeploymentModuleCalldata The data to be passed to the post deployment module
+     *
+     * @return controlledContractAddress The address of the deployed controlled contract.
+     * @return ownerContractAddress The address of the deployed owner contract.
+     */
+    function computeAddresses(
+        ControlledContractDeployment calldata controlledContractDeployment,
+        OwnerContractDeployment calldata ownerContractDeployment,
+        address postDeploymentModule,
+        bytes calldata postDeploymentModuleCalldata
+    )
+        external
+        view
+        returns (
+            address controlledContractAddress,
+            address ownerContractAddress
+        );
+
+    /**
+     * @dev Computes the addresses of the controlled and owner contract proxies to be created.
+     *
+     * @param controlledContractDeploymentInit Contains the needed parameter to deploy a proxy contract. (`salt`, `fundingAmount`, `implementationContract`, `initializationCalldata`)
+     * @param ownerContractDeploymentInit Contains the needed parameter to deploy the owner proxy contract. (`fundingAmount`, `implementationContract`, `addControlledContractAddress`, `initializationCalldata`, `extraInitializationParams`)
+     * @param postDeploymentModule The module to be executed after deployment.
+     * @param postDeploymentModuleCalldata The data to be passed to the post deployment module.
+     *
+     * @return controlledContractAddress The address of the deployed controlled contract proxy
+     * @return ownerContractAddress The address of the deployed owner contract proxy
+     */
+    function computeERC1167Addresses(
+        ControlledContractDeploymentInit
+            calldata controlledContractDeploymentInit,
+        OwnerContractDeploymentInit calldata ownerContractDeploymentInit,
+        address postDeploymentModule,
+        bytes calldata postDeploymentModuleCalldata
+    )
+        external
+        view
+        returns (
+            address controlledContractAddress,
+            address ownerContractAddress
+        );
+}
---
+++ tests/LSP23MultiChainDeployment/LSP23MultiChainDeployment.behaviour.ts
@@ -0,0 +1,151 @@
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { OwnerControlledContractDeployer } from '../../typechain-types';
+import { ERC725YDataKeys } from '../../constants.ts';
+import { calculateProxiesAddresses } from './helpers';
+
+describe('UniversalProfileDeployer', function () {
+  it('should deploy proxies for Universal Profile and Key Manager', async function () {
+    const [allPermissionsSigner, universalReceiver, recoverySigner] = await ethers.getSigners();
+
+    const KeyManagerInitFactory = await ethers.getContractFactory('LSP6KeyManagerInit');
+    const keyManagerInit = await KeyManagerInitFactory.deploy();
+
+    const UniversalProfileInitFactory = await ethers.getContractFactory('UniversalProfileInit');
+    const universalProfileInit = await UniversalProfileInitFactory.deploy();
+
+    const OwnerControlledContractDeployerFactory = await ethers.getContractFactory(
+      'OwnerControlledContractDeployer',
+    );
+
+    const ownerControlledContractDeployer = await OwnerControlledContractDeployerFactory.deploy();
+
+    const UPDelegatorPostDeploymentManagerFactory = await ethers.getContractFactory(
+      'UniversalProfileInitPostDeploymentModule',
+    );
+
+    const upPostDeploymentModule = await UPDelegatorPostDeploymentManagerFactory.deploy();
+
+    const salt = ethers.utils.randomBytes(32);
+
+    const ownerControlledDeploymentInit: OwnerControlledContractDeployer.ControlledContractDeploymentInitStruct =
+      {
+        salt,
+        value: 0,
+        implementationContract: universalProfileInit.address,
+        initializationCalldata: universalProfileInit.interface.encodeFunctionData('initialize', [
+          upPostDeploymentModule.address,
+        ]),
+      };
+
+    const ownerDeploymentInit: OwnerControlledContractDeployer.OwnerContractDeploymentInitStruct = {
+      value: 0,
+      implementationContract: keyManagerInit.address,
+      addControlledContractAddress: true,
+      initializationCalldata: '0xc4d66de8',
+      extraInitializationParams: '0x',
+    };
+
+    const allPermissionsSignerPermissionsKey =
+      '0x4b80742de2bf82acb3630000' + allPermissionsSigner.address.slice(2);
+
+    const universalReceiverPermissionsKey =
+      '0x4b80742de2bf82acb3630000' + universalReceiver.address.slice(2);
+
+    const recoveryAddressPermissionsKey =
+      '0x4b80742de2bf82acb3630000' + recoverySigner.address.slice(2);
+
+    const allPermissionsSignerPermissionsValue =
+      '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
+
+    const create16BytesUint = (value: number) => {
+      return ethers.utils.hexZeroPad(ethers.utils.hexlify(value), 16).slice(2);
+    };
+
+    const types = ['bytes32[]', 'bytes[]'];
+
+    const encodedBytes = ethers.utils.defaultAbiCoder.encode(types, [
+      [
+        ERC725YDataKeys.LSP3.LSP3Profile, // LSP3Metadata
+        ERC725YDataKeys.LSP1.LSP1UniversalReceiverDelegate, // URD Address
+        universalReceiverPermissionsKey, // URD Permissions
+        recoveryAddressPermissionsKey, // Recovery Address permissions
+        allPermissionsSignerPermissionsKey, // Signers permissions
+        ERC725YDataKeys.LSP6['AddressPermissions[]'].length, // Number of address with permissions
+        ERC725YDataKeys.LSP6['AddressPermissions[]'].index + create16BytesUint(0), // Index of the first address
+        ERC725YDataKeys.LSP6['AddressPermissions[]'].index + create16BytesUint(1), // Index of the second address
+        ERC725YDataKeys.LSP6['AddressPermissions[]'].index + create16BytesUint(2), // Index of the third address
+      ],
+      [
+        ethers.utils.randomBytes(32), // LSP3Metadata
+        universalReceiver.address, // URD Address
+        allPermissionsSignerPermissionsValue, // URD Permissions
+        allPermissionsSignerPermissionsValue, // Recovery Address permissions
+        allPermissionsSignerPermissionsValue, // Signers permissions
+        ethers.utils.defaultAbiCoder.encode(['uint256'], [3]), // Address Permissions array length
+        universalReceiver.address,
+        recoverySigner.address,
+        allPermissionsSigner.address,
+      ],
+    ]);
+
+    // get the address of the UP and the KeyManager contracts
+    const [upAddress, keyManagerAddress] =
+      await ownerControlledContractDeployer.callStatic.deployERC1167Proxies(
+        ownerControlledDeploymentInit,
+        ownerDeploymentInit,
+        upPostDeploymentModule.address,
+        encodedBytes,
+      );
+
+    await ownerControlledContractDeployer.deployERC1167Proxies(
+      ownerControlledDeploymentInit,
+      ownerDeploymentInit,
+      upPostDeploymentModule.address,
+      encodedBytes,
+    );
+
+    const upProxy = UniversalProfileInitFactory.attach(upAddress);
+    const keyManagerProxy = KeyManagerInitFactory.attach(keyManagerAddress);
+
+    const upProxyOwner = await upProxy.owner();
+    const keyManagerProxyOwner = await keyManagerProxy.target();
+
+    const [expectedUpProxyAddress, expectedKeyManagerProxyAddress] =
+      await ownerControlledContractDeployer.computeERC1167Addresses(
+        ownerControlledDeploymentInit.salt,
+        ownerControlledDeploymentInit.implementationContract,
+        ownerDeploymentInit.implementationContract,
+        ownerDeploymentInit.initializationCalldata,
+        ownerDeploymentInit.addControlledContractAddress,
+        ownerDeploymentInit.extraInitializationParams,
+        upPostDeploymentModule.address,
+        encodedBytes,
+      );
+
+    const [calculatedUpProxyAddress, calculatedKMProxyAddress] = await calculateProxiesAddresses(
+      ownerControlledDeploymentInit.salt,
+      ownerControlledDeploymentInit.implementationContract,
+      ownerDeploymentInit.implementationContract,
+      ownerDeploymentInit.initializationCalldata,
+      ownerDeploymentInit.addControlledContractAddress,
+      ownerDeploymentInit.extraInitializationParams,
+      upPostDeploymentModule.address,
+      encodedBytes,
+      ownerControlledContractDeployer.address,
+    );
+
+    expect(upAddress).to.equal(expectedUpProxyAddress);
+    expect(upAddress).to.equal(expectedUpProxyAddress);
+    expect(upAddress).to.equal(calculatedUpProxyAddress);
+
+    expect(keyManagerAddress).to.equal(expectedKeyManagerProxyAddress);
+    expect(keyManagerAddress).to.equal(expectedKeyManagerProxyAddress);
+    expect(keyManagerAddress).to.equal(calculatedKMProxyAddress);
+
+    expect(upProxyOwner).to.equal(keyManagerProxy.address);
+    expect(upProxyOwner).to.equal(keyManagerProxy.address);
+    expect(keyManagerProxyOwner).to.equal(upProxy.address);
+    expect(keyManagerProxyOwner).to.equal(upProxy.address);
+  });
+});
