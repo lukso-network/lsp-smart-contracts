6c7577899587d655d362762e4003d85053febdc6
CJ42 <cavallerajean@gmail.com>
2023-08-08 09:23:36 +0100

    build: add LSP23 in artifacts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -150,6 +150,7 @@ const config: HardhatUserConfig = {
       // ------------------
       'Create2Factory',
       'LSP16UniversalFactory',
+      'LinkedContractsFactory',
     ],
     // Whether to include the TypeChain factories or not.
     // If this is enabled, you need to run the TypeChain files through the TypeScript compiler before shipping to the registry.
