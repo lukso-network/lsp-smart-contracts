309b1a936906f9c3b6a6fe34080cbd3e32f1ff6f
CJ42 <cavallerajean@gmail.com>
2023-08-08 09:19:25 +0100

    fix: inheritance of `LSP8Burnable`

--- contracts/LSP8IdentifiableDigitalAsset/extensions/LSP8BurnableInitAbstract.sol
+++ contracts/LSP8IdentifiableDigitalAsset/extensions/LSP8BurnableInitAbstract.sol
@@ -2,17 +2,19 @@
 pragma solidity ^0.8.4;
 
 import {
-    LSP8IdentifiableDigitalAsset
-} from "../LSP8IdentifiableDigitalAsset.sol";
+    LSP8IdentifiableDigitalAssetInitAbstract
+} from "../LSP8IdentifiableDigitalAssetInitAbstract.sol";
 
 // errors
 import {LSP8NotTokenOperator} from "../LSP8Errors.sol";
 
 /**
- * @dev LSP8 extension (standard version) that allows token holders to destroy both
+ * @dev LSP8 extension (proxy version) that allows token holders to destroy both
  * their own tokens and those that they have an allowance for as an operator.
  */
-abstract contract LSP8Burnable is LSP8IdentifiableDigitalAsset {
+abstract contract LSP8BurnableInitAbstract is
+    LSP8IdentifiableDigitalAssetInitAbstract
+{
     function burn(bytes32 tokenId, bytes memory data) public virtual {
         if (!_isOperatorOrOwner(msg.sender, tokenId)) {
             revert LSP8NotTokenOperator(tokenId, msg.sender);
---
+++ contracts/Mocks/Tokens/LSP8BurnableInitTester.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+// modules
+import {
+    LSP8IdentifiableDigitalAssetInitAbstract
+} from "../../LSP8IdentifiableDigitalAsset/LSP8IdentifiableDigitalAssetInitAbstract.sol";
+import {
+    LSP8BurnableInitAbstract
+} from "../../LSP8IdentifiableDigitalAsset/extensions/LSP8BurnableInitAbstract.sol";
+
+contract LSP8BurnableInitTester is LSP8BurnableInitAbstract {
+    function initialize(
+        string memory name_,
+        string memory symbol_,
+        address newOwner_
+    ) public virtual initializer {
+        LSP8IdentifiableDigitalAssetInitAbstract._initialize(
+            name_,
+            symbol_,
+            newOwner_
+        );
+    }
+}
