60e8b9f08c849b782c1788267f21a4d3babe8c42
b00ste <daniel@lukso.io>
2023-08-15 12:51:21 +0300

    test: fix some tests with non-standard values

--- tests/LSP6KeyManager/Admin/PermissionChangeAddExtensions.test.ts
+++ tests/LSP6KeyManager/Admin/PermissionChangeAddExtensions.test.ts
@@ -684,7 +684,7 @@ export const shouldBehaveLikePermissionChangeOrAddExtensions = (
             ],
             dataValues: [
               extensionB,
-              ethers.utils.hexZeroPad(ethers.utils.hexlify(8), 32),
+              ethers.utils.hexZeroPad(ethers.utils.hexlify(8), 16),
               '0xaabb',
             ],
           };
