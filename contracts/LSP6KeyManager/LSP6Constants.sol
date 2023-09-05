c2d3858b2774a16fd0b0c53fbddaa94121a6fbd3
b00ste.lyx <62855857+b00ste@users.noreply.github.com>
2023-09-05 10:24:58 +0300

    refactor: update LSP6/LSP7/LSP8 interface IDs (#696)

--- contracts/LSP6KeyManager/LSP6Constants.sol
+++ contracts/LSP6KeyManager/LSP6Constants.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 // --- ERC165 interface ids
-bytes4 constant _INTERFACEID_LSP6 = 0x627ca5d3;
+bytes4 constant _INTERFACEID_LSP6 = 0x66918867;
 
 // --- ERC725Y Data Keys
 
