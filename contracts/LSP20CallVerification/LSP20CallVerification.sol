180acccbe0110f8d80666f582dab96f5e3f4d894
CJ42 <cavallerajean@gmail.com>
2023-10-10 16:30:15 +0100

    perf: use `try / catch` in LSP20 to reduce runtime cost
    Co-authored-by: b00ste.lyx <b00ste@users.noreply.github.com>

--- contracts/LSP20CallVerification/LSP20CallVerification.sol
+++ contracts/LSP20CallVerification/LSP20CallVerification.sol
@@ -27,28 +27,26 @@ abstract contract LSP20CallVerification {
     function _verifyCall(
         address logicVerifier
     ) internal virtual returns (bool verifyAfter) {
-        if (logicVerifier.code.length == 0)
+        if (logicVerifier.code.length == 0) {
             revert LSP20EOACannotVerifyCall(logicVerifier);
+        }
 
-        (bool success, bytes memory returnedData) = logicVerifier.call(
-            abi.encodeWithSelector(
-                ILSP20.lsp20VerifyCall.selector,
+        try
+            ILSP20(logicVerifier).lsp20VerifyCall(
                 address(this),
                 msg.sender,
                 msg.value,
                 msg.data
             )
-        );
+        returns (bytes4 magicValue) {
+            if (bytes3(magicValue) != bytes3(ILSP20.lsp20VerifyCall.selector)) {
+                revert LSP20InvalidMagicValue(false, abi.encode(magicValue));
+            }
 
-        _validateCall(false, success, returnedData);
-
-        bytes4 magicValue = abi.decode(returnedData, (bytes4));
-
-        if (bytes3(magicValue) != bytes3(ILSP20.lsp20VerifyCall.selector)) {
-            revert LSP20InvalidMagicValue(false, returnedData);
+            return magicValue[3] == 0x01;
+        } catch (bytes memory errorData) {
+            _revertWithLSP20DefaultError(false, errorData);
         }
-
-        return magicValue[3] == 0x01;
     }
 
     /**
