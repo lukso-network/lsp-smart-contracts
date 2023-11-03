c0090c6a6d7e3473702cd85549a1fa2670b00bff
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2023-11-03 12:44:33 +0000

    chore: bootstrap releases for path: . + add `Version.sol` in file to update version for (#785)
    * chore: bootstrap releases for path: .
    
    * build: add `Version.sol` as extra file to update

--- contracts/Version.sol
+++ contracts/Version.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.4;
 
 abstract contract Version {
+    // DO NOT CHANGE
+    // Comments block below is used by release-please to automatically update the version in this file.
+    // x-release-please-start-version
     string internal constant _VERSION = "0.12.0";
+
+    // x-release-please-end
 
     /**
      * @dev Get the version of the contract.
