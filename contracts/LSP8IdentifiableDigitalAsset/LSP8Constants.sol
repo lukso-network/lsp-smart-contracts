22a79cda86d7c3fc62f4b90ddd1620e130afdb55
YamenMerhi <yamennmerhi@gmail.com>
2023-12-05 16:25:10 +0100

    refactor!: change lsp8 interfaceId

--- contracts/LSP8IdentifiableDigitalAsset/LSP8Constants.sol
+++ contracts/LSP8IdentifiableDigitalAsset/LSP8Constants.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 // --- ERC165 interface ids
-bytes4 constant _INTERFACEID_LSP8 = 0xecad9f75;
+bytes4 constant _INTERFACEID_LSP8 = 0x3a271706;
 
 // --- ERC725Y Data Keys
 
