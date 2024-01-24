c843e3035b0f0db3446e97d179e79e42f397ed76
YamenMerhi <yamennmerhi@gmail.com>
2024-01-24 16:22:59 +0200

    chore: run lint

--- packages/LSP16UniversalFactory/hardhat.config.ts
+++ packages/LSP16UniversalFactory/hardhat.config.ts
@@ -113,9 +113,7 @@ const config: HardhatUserConfig = {
   },
   packager: {
     // What contracts to keep the artifacts and the bindings for.
-    contracts: [
-      "LSP16UniversalFactory"
-    ],
+    contracts: ['LSP16UniversalFactory'],
     // Whether to include the TypeChain factories or not.
     // If this is enabled, you need to run the TypeChain files through the TypeScript compiler before shipping to the registry.
     includeFactories: true,
