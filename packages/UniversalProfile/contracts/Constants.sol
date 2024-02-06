0be0ec35092ec6dba46cd390d8b55f573a1b553b
CJ42 <cavallerajean@gmail.com>
2024-02-06 11:59:46 +0100

    chore: update remappings

--- packages/UniversalProfile/contracts/Constants.sol
+++ packages/UniversalProfile/contracts/Constants.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.0;
 /// @dev This files makes constants related to LSP3 Profile Metadata available to be imported
 /// directly from the UniversalProfile package.
 
-// solhint-disable-next-line no-unused-imports
+// solhint-disable no-unused-import
 import {
     _LSP3_SUPPORTED_STANDARDS_KEY,
     _LSP3_SUPPORTED_STANDARDS_VALUE,
     _LSP3_PROFILE_KEY
 } from "lsp3/contracts/LSP3Constants.sol";
+
+// solhint-enable
