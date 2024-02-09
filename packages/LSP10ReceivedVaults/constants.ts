034e7cc3699651f1122838456ce4e90dde65b6b6
CJ42 <cavallerajean@gmail.com>
2024-02-10 00:26:34 +0100

    refactor: add constants and typings for LSP2

--- packages/LSP10ReceivedVaults/constants.ts
+++ packages/LSP10ReceivedVaults/constants.ts
@@ -1,3 +1,5 @@
+import { LSP2ArrayKey } from 'lsp2';
+
 export const LSP10DataKeys = {
   // keccak256('LSP10VaultsMap') + bytes2(0)
   LSP10VaultsMap: '0x192448c3c0f88c7f238c0000',
@@ -6,5 +8,5 @@ export const LSP10DataKeys = {
   'LSP10Vaults[]': {
     length: '0x55482936e01da86729a45d2b87a6b1d3bc582bea0ec00e38bdb340e3af6f9f06',
     index: '0x55482936e01da86729a45d2b87a6b1d3',
-  },
+  } as LSP2ArrayKey,
 };
--- packages/LSP5ReceivedAssets/constants.ts
+++ packages/LSP5ReceivedAssets/constants.ts
@@ -1,3 +1,5 @@
+import { LSP2ArrayKey } from 'lsp2';
+
 export const LSP5DataKeys = {
   // LSP5ReceivedAssetsMap:<address>  + bytes2(0)
   LSP5ReceivedAssetsMap: '0x812c4334633eb816c80d0000',
@@ -6,5 +8,5 @@ export const LSP5DataKeys = {
   'LSP5ReceivedAssets[]': {
     length: '0x6460ee3c0aac563ccbf76d6e1d07bada78e3a9514e6382b736ed3f478ab7b90b',
     index: '0x6460ee3c0aac563ccbf76d6e1d07bada',
-  },
+  } as LSP2ArrayKey,
 };
