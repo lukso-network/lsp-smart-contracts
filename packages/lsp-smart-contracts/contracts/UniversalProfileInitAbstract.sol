b39b2b485fbbd965d46d1354d406a1eba9eeb1bb
CJ42 <cavallerajean@gmail.com>
2024-02-28 07:46:11 -0700

    chore: fix some Solidity import path compiler errors

--- packages/lsp-smart-contracts/contracts/UniversalProfileInitAbstract.sol
+++ packages/lsp-smart-contracts/contracts/UniversalProfileInitAbstract.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.4;
 
-import "@lukso/universalprofile-contracts/UniversalProfileInitAbstract.sol";
+import "@lukso/universalprofile-contracts/contracts/UniversalProfileInitAbstract.sol";
