4c61866c7c20be78f7acce1fdfb383d17e21cbe9
CJ42 <cavallerajean@gmail.com>
2024-07-25 11:40:30 +0100

    chore: move `deploy/` scripts folder inside `lsp-smart-contract` package + fix some ESM imports

--- packages/lsp-smart-contracts/deploy/009_deploy_lsp8_mintable.ts
+++ packages/lsp-smart-contracts/deploy/009_deploy_lsp8_mintable.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { LSP4_TOKEN_TYPES } from '@lukso/lsp4-contracts/constants';
-import { LSP8_TOKEN_ID_FORMAT } from '@lukso/lsp8-contracts/constants';
+import { LSP4_TOKEN_TYPES } from '@lukso/lsp4-contracts';
+import { LSP8_TOKEN_ID_FORMAT } from '@lukso/lsp8-contracts';
 
 const deployLSP8MintableDeterministic: DeployFunction = async ({
   deployments,
