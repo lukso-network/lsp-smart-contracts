d499453612dfe687aef15c7b26d52c15555fe98f
CJ42 <cavallerajean@gmail.com>
2024-09-03 11:23:46 +0900

    refactor!: remove `Core` contract from LSP8 package

--- packages/lsp8-contracts/contracts/ILSP8IdentifiableDigitalAsset.sol
+++ packages/lsp8-contracts/contracts/ILSP8IdentifiableDigitalAsset.sol
@@ -1,16 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.4;
 
-// interfaces
-import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import {
-    IERC725Y
-} from "@erc725/smart-contracts/contracts/interfaces/IERC725Y.sol";
-
 /**
  * @title Interface of the LSP8 - Identifiable Digital Asset standard, a non-fungible digital asset.
  */
-interface ILSP8IdentifiableDigitalAsset is IERC165, IERC725Y {
+interface ILSP8IdentifiableDigitalAsset {
     // --- Events
 
     /**
