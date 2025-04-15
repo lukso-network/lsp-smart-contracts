11626d012868da06a91f17a10eaa0cd25989da0e
CJ42 <cavallerajean@gmail.com>
2025-04-15 16:43:17 +0200

    test: remove typechain in benchmark tests, utils and fixtures

--- packages/lsp-smart-contracts/tests/utils/context.ts
+++ packages/lsp-smart-contracts/tests/utils/context.ts
@@ -1,17 +1,16 @@
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers.js';
-import { KeyManagerInternalTester, LSP6KeyManager, UniversalProfile } from '../../types/index.js';
 
 export type LSP6TestContext = {
   accounts: SignerWithAddress[];
   mainController: SignerWithAddress;
-  universalProfile: UniversalProfile;
-  keyManager: LSP6KeyManager;
+  universalProfile;
+  keyManager;
   initialFunding?: bigint;
 };
 
 export type LSP6InternalsTestContext = {
   accounts: SignerWithAddress[];
   mainController: SignerWithAddress;
-  universalProfile: UniversalProfile;
-  keyManagerInternalTester: KeyManagerInternalTester;
+  universalProfile;
+  keyManagerInternalTester;
 };
