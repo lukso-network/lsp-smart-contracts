b3d67a9de16cf97f8491ab06eb1ad45b9b00714f
CJ42 <cavallerajean@gmail.com>
2025-10-13 14:28:22 +0100

    build: upgrade to eslint v9

--- config/eslint-config-custom/index.js
+++ config/eslint-config-custom/index.js
@@ -1,22 +1,34 @@
-module.exports = {
-  ignorePatterns: [
-    'artifacts/',
-    'cache/',
-    'dist/',
-    'types/',
-    'typechain/',
-    'contracts.ts',
-    'abi.ts',
-  ],
-  extends: ['turbo', 'prettier'],
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-explicit-any': 'off',
+import eslintConfigPrettier from 'eslint-config-prettier';
+import turboPlugin from 'eslint-plugin-turbo';
+import tseslint from 'typescript-eslint';
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export const eslintconfig = [
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
+  {
+    ignores: ['artifacts/', 'cache/', 'dist/', 'types/', 'typechain/', 'contracts.ts', 'abi.ts'],
   },
-};
+];
+// {
+//   extends: ['turbo', 'prettier'],
+//   plugins: ['@typescript-eslint', 'prettier'],
+//   rules: {
+//     'prettier/prettier': 'error',
+//     '@typescript-eslint/no-explicit-any': 'off',
+//   },
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     sourceType: 'module',
+//     ecmaVersion: 2020,
+//   },
+// };
--- template/eslint.config.mjs
+++ template/eslint.config.mjs
@@ -1,8 +1,3 @@
-// module.exports = {
-//   root: true,
-//   extends: ['custom'],
-// };
-
 import { eslintconfig } from 'eslint-config-custom';
 
 /** @type {import("eslint").Linter.Config} */
